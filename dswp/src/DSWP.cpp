c612694ef300b0159b4ad5ffda60731405c5c0da
Simone Campanoni <simo.xan@gmail.com>
2018-02-19 10:19:32 -0600

    Added the skeleton for DSWP

---
+++ dswp/src/DSWP.cpp
@@ -0,0 +1,103 @@
+#include "llvm/Pass.h"
+#include "llvm/IR/Function.h"
+#include "llvm/IR/BasicBlock.h"
+#include "llvm/IR/Instructions.h"
+#include "llvm/IR/LegacyPassManager.h"
+#include "llvm/Support/raw_ostream.h"
+#include "llvm/Transforms/IPO/PassManagerBuilder.h"
+#include "llvm/Analysis/LoopInfo.h"
+
+#include "PDG.hpp"
+#include "PDGAnalysis.hpp"
+
+using namespace llvm;
+
+namespace llvm {
+
+  struct DSWP : public ModulePass {
+    public:
+      static char ID;
+
+      DSWP() : ModulePass{ID} {}
+
+      bool doInitialization (Module &M) override {
+        return false;
+      }
+
+      bool runOnModule (Module &M) override {
+        errs() << "DSWP for " << M.getName() << "\n";
+
+        /*
+         * Fetch the PDG.
+         */
+        auto graph = getAnalysis<PDGAnalysis>().getPDG();
+
+        /*
+         * Fetch the loop to parallelize
+         */
+        auto loop = fetchLoopToParallelize(M);
+        if (loop == nullptr){
+          return false;
+        }
+
+        /*
+         * Parallelize the loop
+         */
+        auto modified = applyDSWP(loop);
+
+        return modified;
+      }
+
+      void getAnalysisUsage(AnalysisUsage &AU) const override {
+        AU.addRequired<PDGAnalysis>();
+        AU.addRequired<LoopInfoWrapperPass>();
+
+        return ;
+      }
+
+    private:
+      Loop * fetchLoopToParallelize (Module &M){
+
+        /* 
+         * Fetch the entry point of the program.
+         */
+        auto entryFunction = M.getFunction("main");
+
+        /*
+         * Fetch the loops.
+         */
+        auto loops = getAnalysis<LoopInfoWrapperPass>(entryFunction).getLoopInfo();
+
+        /*
+         * Choose the loop to parallelize.
+         */
+        for (auto loopIter : loops){
+          auto loop = &*loopIter;
+          return loop;
+        }
+
+        return nullptr;
+      }
+
+      bool applyDSWP (Loop *loop){
+
+        //TODO
+        
+        return false;
+      }
+  };
+
+}
+
+// Next there is code to register your pass to "opt"
+char llvm::DSWP::ID = 0;
+static RegisterPass<DSWP> X("DSWP", "DSWP parallelization");
+
+// Next there is code to register your pass to "clang"
+static DSWP * _PassMaker = NULL;
+static RegisterStandardPasses _RegPass1(PassManagerBuilder::EP_OptimizerLast,
+    [](const PassManagerBuilder&, legacy::PassManagerBase& PM) {
+        if(!_PassMaker){ PM.add(_PassMaker = new DSWP());}}); // ** for -Ox
+static RegisterStandardPasses _RegPass2(PassManagerBuilder::EP_EnabledOnOptLevel0,
+    [](const PassManagerBuilder&, legacy::PassManagerBase& PM) {
+        if(!_PassMaker){ PM.add(_PassMaker = new DSWP());}});// ** for -O0
