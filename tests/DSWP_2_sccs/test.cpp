c76b7e326624ea17cf7574843bd9d8bf1397be01
Simone Campanoni <simo.xan@gmail.com>
2018-05-11 09:08:47 -0500

    Avoid to have clang to optimize away the whole program for a test

--- tests/DSWP_2_sccs/test.cpp
+++ tests/DSWP_2_sccs/test.cpp
@@ -1,16 +1,26 @@
 #include <stdio.h>
+#include <stdint.h>
 
-int main (){
+int main (int argc, char *argv[]){
   int v1, v2, v1_temp;
-  v1 = 0;
-  v2 = 0;
+  v1 = argc;
+  v2 = ((argc * 5) / 2) + 1;
 
-  for (int i = 0; i < 100; ++i) {
+  for (uint32_t i = 0; i < UINT32_MAX; ++i) {
+
+    // SCC 1
     v1_temp = v1;
     v1 = v1 + 1;
     v1 = v1 - 3;
+
+    // SCC 2
     v2 = v2 - v1_temp;
     v2 = v2 * 3;
+    v2 = v2 / 2;
+    v2 = v2 * v2;
+    v2 = v2 * v2;
+    v2 = v2 * v2;
+    v2 = v2 / 2;
   }
 
   printf("%d, %d\n", v1, v2);
