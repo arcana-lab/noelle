95e5495284d7a170aff5696af92c489209233a57
Simone Campanoni <simo.xan@gmail.com>
2018-05-28 16:42:40 -0500

    Added the ability to install passes anywhere

--- passes/scripts/run_me.sh
+++ passes/scripts/run_me.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
+# Set the installation directory
+installDir=$PDG_INSTALL_DIR ;
+if test "$installDir" == "" ; then
+  installDir="~/CAT"
+fi
+
+# Install
 rm -rf build/ ; 
 mkdir build ; 
 cd build ; 
-cmake3 -DCMAKE_INSTALL_PREFIX="~/CAT" -DCMAKE_BUILD_TYPE=Debug ../ ; 
+cmake3 -DCMAKE_INSTALL_PREFIX="${installDir}" -DCMAKE_BUILD_TYPE=Debug ../ ; 
 make ;
 make install ;
 cd ../ 
