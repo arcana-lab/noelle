6742eaf1ce2ac9e99de58fd522621c8eb24c78fc
Simone Campanoni <simo.xan@gmail.com>
2018-06-21 16:26:24 -0500

    Added more comments

--- passes/dswp/src/InductionVariable.cpp
+++ passes/dswp/src/InductionVariable.cpp
@@ -4,15 +4,26 @@ using namespace llvm;
 
 void DSWP::collectRemovableSCCsByInductionVars (DSWPLoopDependenceInfo *LDI) {
   auto &SE = getAnalysis<ScalarEvolutionWrapperPass>(*LDI->function).getSE();
-  auto &sccSubgraph = LDI->loopSCCDAG;
-  for (auto sccNode : sccSubgraph->getNodes()) {
-    auto scc = sccNode->getT();
-    if (sccNode->numOutgoingEdges() == 0) continue;
+  for (auto sccNode : LDI->loopSCCDAG->getNodes()) {
 
-    bool isRemovableSCC = true;
+    /*
+     * Check if the current node of the SCCDAG is an SCC.
+     */
+    if (sccNode->numOutgoingEdges() == 0) {
+      continue;
+    }
+
+    /*
+     * The current node of the SCCDAG is an SCC.
+     *
+     * Check if this SCC can be removed exploiting induction variables.
+     * In more detail, this SCC can be removed if the loop-carried data dependence, which has created this SCC in the PDG, is due to updates to induction variables.
+     */
+    auto scc = sccNode->getT();
+    auto isRemovableSCC = true;
     for (auto iNodePair : scc->internalNodePairs()) {
       auto V = iNodePair.first;
-      bool canBePartOfRemovableSCC = isa<CmpInst>(V) || isa<TerminatorInst>(V);
+      auto canBePartOfRemovableSCC = isa<CmpInst>(V) || isa<TerminatorInst>(V);
 
       auto scev = SE.getSCEV(V);
       switch (scev->getSCEVType()) {
@@ -30,16 +41,18 @@ void DSWP::collectRemovableSCCsByInductionVars (DSWPLoopDependenceInfo *LDI) {
       case scUnknown:
       case scCouldNotCompute:
         isRemovableSCC &= canBePartOfRemovableSCC;
-        // V->print(errs() << "Is not a removable instruction:\t"); errs() << "\n";
         continue;
       default:
-       llvm_unreachable("Unknown SCEV type!");
+       llvm_unreachable("DSWP: Unknown SCEV type!");
       }
     }
 
-    if (isRemovableSCC) LDI->removableSCCs.insert(scc);
-    // if (isRemovableSCC) scc->print(errs() << "REMOVABLE SCC:\n") << "\n";
+    if (isRemovableSCC) {
+      LDI->removableSCCs.insert(scc);
+    }
   }
+
+  return ;
 }
 
 void DSWP::addRemovableSCCsToStages (DSWPLoopDependenceInfo *LDI) {
