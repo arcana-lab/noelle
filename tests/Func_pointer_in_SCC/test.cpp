9d93ae8c9a7553dd2484be310f9d6bc7f36d42e2
angelomatni <angelomatni2018@u.northwestern.edu>
2018-07-08 18:26:59 -0500

    Add test that breaks SCCAnalysis

---
+++ tests/Func_pointer_in_SCC/test.cpp
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int f(int v) {
+  return v + 2;
+}
+
+int g(int printVar, int v1, int v2, int (*f_f)(int)) {
+  for (int i = 0; i < 100; ++i) {
+    v1 -= 1;
+    printVar += (*f_f)(v1);
+    v1 += printVar;
+    printVar -= v1;
+    v2 += v1 + printVar;
+  }
+
+  printf("%d %d %d\n", printVar, v1, v2);
+}
+
+int main (){
+  int v1, v2;
+  int printVar;
+  v1 = 111;
+  printVar = 0;
+  v2 = 4;
+  auto *f_f = &f;
+
+  g(printVar, v1, v2, f_f);
+
+  return 0;
+}
