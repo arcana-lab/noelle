fd7054f203bd2bce78194b6ddf326a7786792e8d
angelomatni <angelomatni2018@u.northwestern.edu>
2018-07-31 22:14:32 -0500

    Planning: thoughts for approaching DOALL implementation

--- passes/dswp/src/DOALL.cpp
+++ passes/dswp/src/DOALL.cpp
@@ -4,7 +4,23 @@ using namespace llvm;
 
 bool DSWP::applyDOALL (DSWPLoopDependenceInfo *LDI, Parallelization &par, Heuristics *h) {
   //TODO
-  abort();
+  errs() << "DSWP:   IS DO ALL LOOP: ----------------------------\n";
 
-  return true;
+  /*
+   * Collect environment information
+   */
+
+  // Thoughts: start by copy pasta-ing environment and stage builder functionality
+  // Use DOALL specific data structures, even if they share fields with DSWP ones
+  // Find the overlap later and refactor
+
+  // TODO: Adapt for DOALL usage
+  // collectPreLoopEnvInfo(LDI);
+  // LDI->envArrayType = ArrayType::get(PointerType::getUnqual(par.int8), LDI->environment->envSize());
+
+  /*
+   * Create DOALL chunking function
+   */
+
+  return false;
 }
