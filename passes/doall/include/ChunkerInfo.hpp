d3645d176129a8cf694d2b2fe9bb18ec3b2f4e9f
angelomatni <angelomatni2018@u.northwestern.edu>
2018-08-25 13:48:11 -0500

    wip+Refactor: doall: separate out utility from algorithm in chunk creation

---
+++ passes/doall/include/ChunkerInfo.hpp
@@ -0,0 +1,62 @@
+#pragma once;
+
+#include "llvm/IR/Function.h"
+#include "llvm/IR/BasicBlock.h"
+#include "llvm/IR/Instructions.h"
+
+#include <unordered_map>
+
+namespace llvm {
+  struct ChunkerInfo {
+    Function *f;
+    BasicBlock *entryBlock, *exitBlock;
+
+    /*
+     * Chunking (outer) loop header/latch
+     */
+    BasicBlock *chHeader, *chLatch;
+
+    /*
+     * Chunking function arguments
+     */
+    Value *envArgVal, *coreArgVal;
+    Value *numCoresArgVal, *chunkSizeArgVal;
+
+    /*
+     * Map from original to clone:
+     *  inner loop values
+     *  environment values
+     *  inner loop bb structure
+     */
+    unordered_map<Instruction *, Instruction *> innerValMap;
+    unordered_map<Value *, Value *> preEnvMap;
+    unordered_map<BasicBlock *, BasicBlock *> innerBBMap;
+
+    /*
+     * Inner loop IV values
+     */
+    BranchInst *originHeaderBr;
+    CmpInst *originCmp;
+    PHINode *originIV;
+    Value *maxIV;
+    int originCmpPHIIndex, originCmpMaxIndex;
+
+    Value *stepperIV;
+    ConstantInt *originStepSize;
+    int stepSizeIVIndex;
+
+    Value *starterIV;
+    int startValIVIndex
+
+    /*
+     * Utility values/functions
+     */
+    Value *zeroV;
+
+    BasicBlock *createChunkerBB () {
+      auto &cxt = f->getContext();
+      return BasicBlock::Create(cxt, "", f);
+    }
+
+  }
+}
