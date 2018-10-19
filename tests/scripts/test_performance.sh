79e7f521397dec2cab39270a1c35ce63e867b444
Simone Campanoni <simo.xan@gmail.com>
2018-10-19 09:57:29 -0500

    Performance tests: Improved the performance tests to show the limit of DOALL and DSWP

--- tests/scripts/test_performance.sh
+++ tests/scripts/test_performance.sh
@@ -21,18 +21,6 @@ function runningTests {
     # echo "   Make " ;
     make PARALLELIZATION_OPTIONS="$2" >> compiler_output.txt 2>&1 ;
 
-    # Capture output only
-    # echo "   Small run " ;
-    ./baseline 10 &> output_baseline.txt ;
-    ./parallelized 10 &> output_parallelized.txt ;
-
-    # Check the output to confirm correctness before measuring performance
-    cmp output_baseline.txt output_parallelized.txt &> /dev/null ;
-    if test $? -ne 0 ; then
-      echo "Performance test failed" ;
-      return 1 ;
-    fi
-
     # Read input for arguments to performance runs
     local ARGS=$(< perf_args.info) ;
 
@@ -61,8 +49,6 @@ function runningTests {
 }
 
 cd performance ;
-runningTests "Measuring with \"-dswp-force -dswp-no-scc-merge\"" "-dswp-verbose=0 -dswp-force -dswp-no-scc-merge" "speedups-no-merge.txt" ;
-runningTests "Measuring with \"-dswp-force\"" "-dswp-verbose=0 -dswp-force" "speedups-force.txt" ;
 runningTests "Measuring the default configuration" "-dswp-verbose=0" "speedups.txt" ;
 cd ../ ;
 
