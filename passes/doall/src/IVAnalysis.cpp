6444973943db9bbc47ccc0315d7bc5137784b7e4
Simone Campanoni <simo.xan@gmail.com>
2018-10-19 12:00:24 -0500

    DOALL: added more comments

--- passes/doall/src/IVAnalysis.cpp
+++ passes/doall/src/IVAnalysis.cpp
@@ -8,12 +8,18 @@ void DOALL::reduceOriginIV (
   ScalarEvolution &SE
 ) {
 
+  /*
+   * Fetch the information about the loop induction variable that controls the loop trip count.
+   */
   auto headerBr = LDI->header->getTerminator();
   auto headerSCC = LDI->loopSCCDAG->sccOfValue(headerBr);
   auto &attrs = LDI->sccdagAttrs.getSCCAttrs(headerSCC);
   assert(attrs->isSimpleIV);
   chunker->originIVAttrs = attrs.get();
 
+  /*
+   * Map the original instructions related to the loop induction variable to the cloned ones in the parallelized loop.
+   */
   chunker->cloneIV = cast<PHINode>(chunker->fetchClone(attrs->singlePHI));
   auto IVInfo = attrs->simpleIVInfo;
   chunker->cloneIVInfo.cmp = cast<CmpInst>(chunker->fetchClone(IVInfo.cmp));
@@ -27,7 +33,7 @@ void DOALL::reduceOriginIV (
 
   auto oneV = ConstantInt::get(IVInfo.step->getType(), 1);
   chunker->cloneIVInfo.step = oneV;
-  int stepSize = IVInfo.step->getValue().getSExtValue();
+  auto stepSize = IVInfo.step->getValue().getSExtValue();
   if (stepSize == 1) {
     chunker->cloneIVInfo.start = startClone;
     chunker->cloneIVInfo.cmpIVTo = endClone;
