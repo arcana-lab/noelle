21afda405f695b8ee8da0ddaae3e7c355003dfb7
angelomatni <angelomatni2018@u.northwestern.edu>
2018-10-20 15:23:24 -0500

    Minor: doall: explain how the bounds, [start, end), of the original loop IV are determined

--- passes/doall/src/IVAnalysis.cpp
+++ passes/doall/src/IVAnalysis.cpp
@@ -18,19 +18,43 @@ void DOALL::reduceOriginIV (
   chunker->originIVAttrs = attrs.get();
 
   /*
-   * Map the original instructions related to the loop induction variable to the cloned ones in the parallelized loop.
+   * Map from instructions of the original loop's IV to the respective cloned instructions.
    */
   chunker->cloneIV = cast<PHINode>(chunker->fetchClone(attrs->singlePHI));
   auto IVInfo = attrs->simpleIVInfo;
   chunker->cloneIVInfo.cmp = cast<CmpInst>(chunker->fetchClone(IVInfo.cmp));
   chunker->cloneIVInfo.br = cast<BranchInst>(chunker->fetchClone(IVInfo.br));
 
+  /*
+   * ============================================================================
+   * Task: Determine the bounds [start, end) of the original loop's IV
+   * ============================================================================
+   */
+
+  /*
+   * Fetch clone of initial Value of the original loop's IV PHINode: [start, ...)
+   */
   auto startClone = chunker->fetchClone(IVInfo.start);
-  auto endClone = chunker->fetchClone(IVInfo.cmpIVTo);
+
+  /*
+   * Fetch clone of Value used in CmpInst of the original loop's IV
+   */
+  auto cmpToClone = chunker->fetchClone(IVInfo.cmpIVTo);
+
+  /*
+   * Fetch the offset from the compared to Value to the end value: [..., end)
+   * cmpToValue + offset = end
+   */
   auto offsetV = ConstantInt::get(IVInfo.step->getType(), IVInfo.endOffset);
   IRBuilder<> entryB(chunker->entryBlock);
   endClone = IVInfo.endOffset ? entryB.CreateAdd(endClone, offsetV) : endClone;
 
+  /*
+   * Manipulate clone IV [start, end) values to guarantee the following: 
+   * 1) The PHI begins with the start value
+   * 2) The PHI is incremented/decremented at the loop latch, NOT before
+   * 3) The CmpInst checks that the end value is NOT reached. If it is, the loop body is NOT executed
+   */
   auto oneV = ConstantInt::get(IVInfo.step->getType(), 1);
   chunker->cloneIVInfo.step = oneV;
   auto stepSize = IVInfo.step->getValue().getSExtValue();
