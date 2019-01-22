8e9f861533cb6153fadf6353c4031b478aa6432f
Simone Campanoni <simo.xan@gmail.com>
2019-01-22 08:34:27 -0600

    Fixed the script to download

--- tests/scripts/download.sh
+++ tests/scripts/download.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 
-for sw in "$@" ; do
-  swName=`basename $sw` ;
-  if ! test -d "$swName" ; then
-    echo "Cloning $swName" ;
-    git clone "$sw" ;
-    continue ;
-  fi
+# Fetch the inputs
+if test $# -lt 2 ; then
+  echo "USAGE: `basename $0` HOST_AND_DIR_TO_USE DEST_DIR" ;
+  exit 1;
+fi
+hostAndDir="$1" ;
+destDir="$2" ;
 
-  echo "Updating $swName" ;
+# Check if the destination directory already exist
+if test -d "$destDir" ; then
+  echo "Updating $destDir" ;
   pushd ./ ;
-  cd "$swName" ;
+  cd "$destDir" ;
   git pull ;
   popd ;
-done
+  exit 0;
+fi
+
+# Clone the repository
+echo "Cloning ${hostAndDir}" ;
+git clone "${hostAndDir}" "$destDir" ;
