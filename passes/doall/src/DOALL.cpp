9b37ae0e453f1b09f6c0f2e8938ac03a68c6d97b
Simone Campanoni <simo.xan@gmail.com>
2019-04-02 09:56:55 -0500

    DOALL: added more printing

--- passes/doall/src/DOALL.cpp
+++ passes/doall/src/DOALL.cpp
@@ -119,7 +119,14 @@ bool DOALL::canBeAppliedToLoop (
     if (this->verbose != Verbosity::Disabled) {
       errs() << "DOALL:   We found an SCC of type " << scc->getType() << " of the loop that is non clonable and non commutative\n" ;
       if (this->verbose >= Verbosity::Maximal) {
-        scc->printMinimal(errs(), "DOALL:\t") << "\n";
+        scc->printMinimal(errs(), "DOALL:     ") << "\n";
+        errs() << "DOALL:     Loop-carried data dependences\n";
+        LDI->sccdagAttrs.iterateOverLoopCarriedDataDependences(scc, [](DGEdge<Value> *dep) -> bool {
+          auto fromInst = dep->getOutgoingT();
+          auto toInst = dep->getIncomingT();
+          errs() << "DOALL:       " << *fromInst << " ---> " << *toInst << "\n";
+          return false;
+            });
       }
     }
     return false;
