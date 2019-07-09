1a2afe22395e489675ab2f4d922a443f5cf1030e
Joe Isaacs <joe.isaacs@cl.cam.ac.uk>
2019-07-09 15:59:54 +0100

    Added VIALoop definition

--- passes/unique_ir_marker/include/UniqueIRConstants.hpp
+++ passes/unique_ir_marker/include/UniqueIRConstants.hpp
@@ -10,6 +10,7 @@ class UniqueIRConstants {
  public:
   static const StringRef VIAInstruction;
   static const StringRef VIABasicBlock;
+  static const StringRef VIALoop;
   static const StringRef VIAFunction;
   static const StringRef VIAModule;
 };
