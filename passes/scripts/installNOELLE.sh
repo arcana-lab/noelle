d5c4f821a0b99ddf279f1defc838ab5d21bdf5c6
Simone Campanoni <simo.xan@gmail.com>
2019-08-01 09:41:59 -0500

    Added the noelle command

---
+++ passes/scripts/installNOELLE.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Set the installation directory
+installDir=$PDG_INSTALL_DIR ;
+if test "$installDir" == "" ; then
+  eval installDir="~/CAT"
+fi
+
+mkdir -p ${installDir}/bin ;
+
+awk -v installDirectory="${installDir}" '{
+    if ($1 == "installDir"){
+      printf("%s=%s\n", $1, installDirectory);
+    } else {
+      print ;
+    }
+  }' scripts/noelle > ${installDir}/bin/noelle ;
+
+chmod 744 ${installDir}/bin/noelle ;
