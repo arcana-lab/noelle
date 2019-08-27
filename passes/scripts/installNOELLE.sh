767b75e59ae007a7ec73d7ea8fc101faf28c5806
Simone Campanoni <simo.xan@gmail.com>
2019-08-27 09:45:38 -0500

    Added noelle-pgo

--- passes/scripts/installNOELLE.sh
+++ passes/scripts/installNOELLE.sh
@@ -24,6 +24,15 @@ awk -v installDirectory="${installDir}" '{
     }
   }' scripts/noelle-pre > ${installDir}/bin/noelle-pre ;
 
+awk -v installDirectory="${installDir}" '{
+    if ($1 == "installDir"){
+      printf("%s=\"%s\"\n", $1, installDirectory);
+    } else {
+      print ;
+    }
+  }' scripts/noelle-pgo > ${installDir}/bin/noelle-pgo ;
+
 
 chmod 744 ${installDir}/bin/noelle ;
 chmod 744 ${installDir}/bin/noelle-pre ;
+chmod 744 ${installDir}/bin/noelle-pgo ;
