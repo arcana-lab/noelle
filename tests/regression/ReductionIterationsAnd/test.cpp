72e739fa6eb293a8ead101d7c433dfdf28f70925
Simone Campanoni <simo.xan@gmail.com>
2019-11-04 14:47:16 -0600

    Added two more tests

---
+++ tests/regression/ReductionIterationsAnd/test.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+long long int computation (long long int *a, long long int iters){
+  long long int s =0;
+
+  for (auto i=0; i < iters; ++i){
+    s &=  a[i];
+  }
+
+  return s;
+}
+
+int main (int argc, char *argv[]){
+
+  /*
+   * Check the inputs.
+   */
+  if (argc < 2){
+    fprintf(stderr, "USAGE: %s LOOP_ITERATIONS\n", argv[0]);
+    return -1;
+  }
+  auto iterations = atoll(argv[1]);
+  long long int *array = (long long int *) calloc(iterations, sizeof(long long int));
+
+  auto s = computation(array, iterations);
+  printf("%lld\n", s);
+
+  return 0;
+}
