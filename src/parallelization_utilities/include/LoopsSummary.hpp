a536aa3ed414ce653820902b4b383b394d7f8fce
Simone Campanoni <simo.xan@gmail.com>
2019-11-05 22:09:25 -0600

    Added missing files

---
+++ src/parallelization_utilities/include/LoopsSummary.hpp
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2016 - 2019  Angelo Matni, Simone Campanoni
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. 
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+#pragma once
+
+#include "SystemHeaders.hpp"
+#include "LoopSummary.hpp"
+
+namespace llvm {
+
+  class LoopsSummary {
+    public:
+      LoopSummary *topLoop;
+      std::set<std::unique_ptr<LoopSummary>> loops;
+
+      LoopSummary * getLoop (Instruction *instIncludedInLoop);
+
+      LoopSummary * getLoop (BasicBlock *bbIncludedInLoop);
+
+      void populate (LoopInfo &li, Loop *loop);
+
+      void print (raw_ostream &stream);
+
+    private:
+
+      /*
+       * Fields.
+       */
+      std::unordered_map<BasicBlock *, LoopSummary *> bbToLoop;
+
+      /*
+       * Methods.
+       */
+      LoopSummary *createSummary (Loop *l);
+  };
+
+}
---
+++ src/parallelization_utilities/src/LoopsSummary.cpp
@@ -0,0 +1,80 @@
+/*
+ * Copyright 2016 - 2019  Angelo Matni, Simone Campanoni
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. 
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+#include "LoopsSummary.hpp"
+
+using namespace llvm;
+      
+LoopSummary * LoopsSummary::getLoop (Instruction *instIncludedInLoop){
+  auto instBB = instIncludedInLoop->getParent();
+  auto l = this->getLoop(instBB);
+  return l;
+}
+
+LoopSummary * LoopsSummary::getLoop (BasicBlock *bbIncludedInLoop){
+  auto lIter = this->bbToLoop.find(bbIncludedInLoop);
+  if (lIter == this->bbToLoop.end()){
+    return nullptr;
+  }
+  auto l = this->bbToLoop[bbIncludedInLoop];
+  return l;
+}
+
+LoopSummary *LoopsSummary::createSummary (Loop *l) {
+  auto id = loops.size();
+  auto lSummary = std::make_unique<LoopSummary>(id, l);
+  auto lPtr = lSummary.get();
+  for (auto bb : l->blocks()) {
+    bbToLoop[bb] = lPtr;
+  }
+  auto ls = loops.insert(std::move(lSummary)).first->get();
+
+  return ls;
+}
+      
+void LoopsSummary::populate(LoopInfo &li, Loop *loop) {
+  std::unordered_map<Loop *, LoopSummary *> loopToSummary;
+  loopToSummary[loop->getParentLoop()] = nullptr;
+
+  /*
+   * NOTE(angelo): subloops only include 1-level deep loops
+   *  entirely contained within the top level loop
+   */
+  std::queue<Loop *> toSummarize;
+  toSummarize.push(loop);
+
+  while (!toSummarize.empty()) {
+    auto l = toSummarize.front();
+    toSummarize.pop();
+
+    auto summary = this->createSummary(l);
+    loopToSummary[l] = summary;
+    auto parent = l->getParentLoop();
+    assert(loopToSummary.find(parent) != loopToSummary.end());
+    summary->parent = loopToSummary[parent];
+
+    for (auto subLoop : l->getSubLoops()) {
+      toSummarize.push(subLoop);
+    }
+  }
+
+  topLoop = loopToSummary[loop];
+
+  return ;
+}
+
+void LoopsSummary::print (raw_ostream &stream) {
+  stream << "Loop summaries:\n";
+  for (auto &loop : loops) {
+    loop->print(stream);
+  }
+
+  return ;
+}
