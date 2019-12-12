0573190bc69ac541dc66245a97c04391587fbf93
Simone Campanoni <simo.xan@gmail.com>
2019-12-12 09:27:24 -0600

    Added some comments

--- src/parallelization_utilities/src/SCCDAGAttrs.cpp
+++ src/parallelization_utilities/src/SCCDAGAttrs.cpp
@@ -76,8 +76,25 @@ std::set<SCC *> SCCDAGAttrs::getSCCsWithLoopCarriedDependencies (void) const {
       
 std::set<SCC *> SCCDAGAttrs::getSCCsWithLoopCarriedControlDependencies (void) const {
   std::set<SCC *> sccs;
+
+  /*
+   * Iterate over SCCs with loop-carried data dependences.
+   */
   for (auto &sccDependencies : this->interIterDeps) {
+
+    /*
+     * Fetch the SCC.
+     */
+    auto SCC = sccDependencies.first;
+
+    /*
+     * Fetch the set of loop-carried data dependences of the current SCC.
+     */
     auto &deps = sccDependencies.second;
+
+    /*
+     * Check if this SCC has a control loop-carried data dependence.
+     */
     auto isControl = false;
     for (auto dep : deps){
       if (dep->isControlDependence()){
@@ -89,13 +106,31 @@ std::set<SCC *> SCCDAGAttrs::getSCCsWithLoopCarriedControlDependencies (void) co
       sccs.insert(sccDependencies.first);
     }
   }
+
   return sccs;
 }
 
 std::set<SCC *> SCCDAGAttrs::getSCCsWithLoopCarriedDataDependencies (void) const {
   std::set<SCC *> sccs;
+
+  /*
+   * Iterate over SCCs with loop-carried data dependences.
+   */
   for (auto &sccDependencies : this->interIterDeps) {
+
+    /*
+     * Fetch the SCC.
+     */
+    auto SCC = sccDependencies.first;
+
+    /*
+     * Fetch the set of loop-carried data dependences of the current SCC.
+     */
     auto &deps = sccDependencies.second;
+
+    /*
+     * Check if this SCC has data loop-carried data dependence.
+     */
     auto isData = false;
     for (auto dep : deps){
       if (dep->isDataDependence()){
@@ -104,7 +139,7 @@ std::set<SCC *> SCCDAGAttrs::getSCCsWithLoopCarriedDataDependencies (void) const
       }
     }
     if (isData){
-      sccs.insert(sccDependencies.first);
+      sccs.insert(SCC);
     }
   }
   return sccs;
