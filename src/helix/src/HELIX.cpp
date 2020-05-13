c988c303cbe54d64ee80eab8ba91b28756316445
angelomatni <angelomatni340@gmail.com>
2020-05-12 18:35:43 -0600

    HELIX: Store reducible value even if SS short circuits
    And populate initial value for reducible cloned values in task

--- src/helix/src/HELIX.cpp
+++ src/helix/src/HELIX.cpp
@@ -227,6 +227,7 @@ void HELIX::createParallelizableTask (
    * Fix the data flow within the parallelized loop by redirecting operands of cloned instructions to refer to the other cloned instructions. 
    */
   this->adjustDataFlowToUseClones(LDI, 0);
+  this->setReducableVariablesToBeginAtIdentityValue(LDI, 0);
 
   /*
    * Add the unconditional branch from the entry basic block to the header of the loop.
@@ -235,16 +236,20 @@ void HELIX::createParallelizableTask (
   entryBuilder.CreateBr(helixTask->basicBlockClones[LDI->header]);
 
   /*
-   * Add the final return instruction to the single task's exit block.
-   */
-  IRBuilder<> exitB(helixTask->exitBlock);
-  exitB.CreateRetVoid();
-
-  /*
    * Store final results of loop live-out variables. 
    * Note this occurs after data flow is adjusted.  TODO: is this a must? if so, let's say it explicitely
    */
   this->generateCodeToStoreLiveOutVariables(LDI, 0);
+
+  /*
+   * HACK: Hoist reducible live out StoreInst right before the task's return statement
+   * since short circuiting synchronization logic skips loop exit blocks. The fix is to either
+   * 1) store all live outs (cloned, reducible, or from spilled environment) right before the task's return
+   *  This would require re-loading of spilled environment and cause redundant stores to spilled environment
+   * 2) initially store reducible live outs right before the task's return
+   *  This is slightly messier code-wise (deviating from ParallelizationTechnique's implementation)
+   */
+  this->hoistReducibleLiveOutStoresToTaskExit(LDI);
 
   /*
    * Generate a store to propagate information about which exit block the parallelized loop took.
@@ -255,6 +260,12 @@ void HELIX::createParallelizableTask (
    * Spill loop carried dependencies into a separate environment array
    */
   this->spillLoopCarriedDataDependencies(LDI);
+
+  /*
+   * Add the final return instruction to the single task's exit block.
+   */
+  IRBuilder<> exitB(helixTask->exitBlock);
+  exitB.CreateRetVoid();
 
   if (this->verbose >= Verbosity::Maximal) {
     SubCFGs execGraph(*helixTask->F);
