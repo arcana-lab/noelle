1a9c34fcc26aa5388eaa95249051dfbf99b2d877
Simone Campanoni <simo.xan@gmail.com>
2020-06-17 23:21:08 -0500

    Install unit libraries in the default installation directory

--- tests/scripts/build_unit.sh
+++ tests/scripts/build_unit.sh
@@ -1,25 +1,21 @@
 #!/bin/bash -e
 
-function buildSuite {
-  if ! test -d $1 ; then
-    return ;
+for i in `ls` ; do
+
+  # Check if this is a directory
+  if ! test -d "$i"; then
+    continue ;
   fi
 
-  if [ ! -f $1/CMakeLists.txt ]; then
-    return ;
+  # Skip install dir
+  if test "$i" == "install" ; then
+    continue ;
   fi
+  echo $i
 
-  cd $1 ;
-
-  echo -e " Building unit test suite: $i" ;
-  rm -rf build/ ; 
-  mkdir build ; 
-  cd build ; 
-  cmake3 -DCMAKE_INSTALL_PREFIX=$PDG_INSTALL_DIR/test -DCMAKE_BUILD_TYPE=Debug ../ ; 
-  make && make install;
-  cd ../
-
-  cd ../
-}
-
-buildSuite $1 ;
+  # Compile 
+  pushd ./ ;
+  cd "$i" ;
+  ../../../src/scripts/run_me.sh ;
+  popd ;
+done
