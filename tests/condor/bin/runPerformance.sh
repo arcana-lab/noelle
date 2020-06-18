abad7aa942934ac5d8711676488f5fa8a8aa4aca
Simone Campanoni <simo.xan@gmail.com>
2020-06-18 11:14:08 -0500

    Included the performance tests in condor

---
+++ tests/condor/bin/runPerformance.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -e
+
+# Fetch the inputs
+if test $# -lt 1 ; then
+  echo "USAGE: `basename $0` UNITTEST_DIR" ;
+  exit 1;
+fi
+testDir="$1" ;
+
+# Setup the environment
+source ~/.bash_profile ;
+
+# Go to the directory
+cd $testDir ;
+
+# Run
+make performance > compiler_output_performance.txt 2>&1 ;
