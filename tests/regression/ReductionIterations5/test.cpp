127f4a10493e5d794ba20913bd784d76d9ccc4ba
Simone Campanoni <simo.xan@gmail.com>
2020-06-25 15:32:09 -0500

    Tests: improved a test

--- tests/regression/ReductionIterations5/test.cpp
+++ tests/regression/ReductionIterations5/test.cpp
@@ -2,19 +2,15 @@
 #include <stdlib.h>
 #include <math.h>
 
-void computeSum (long long int *a, long long int SIZE_Z, long long int SIZE_X, long long int SIZE_Y){
-	int x, y,  z;
-
-  x = SIZE_X;
-  y = SIZE_Y;
-  for(z = 0; z < SIZE_Z; z++){
+void computeSum (long long int *a, long long int SIZE_Z){
+  for(int z = 0; z < SIZE_Z; z++){
     if (z == 0){
-      for (int j=0; j < 1000000; j++){
+      for (int j=0; j < 1000000000; j++){
         rand();
       }
     }
 
-    (*a) += x + y + z;
+    (*a) += z;
   }
 
   return ;
@@ -32,10 +28,9 @@ int main (int argc, char *argv[]){
   auto iterations = atoll(argv[1]);
   if (iterations == 0) return 0;
 
-  iterations *= 100;
   long long int *array = (long long int *) calloc(iterations, sizeof(long long int));
 
-  computeSum(&(array[0]), iterations, 2, 2);
+  computeSum(&(array[0]), iterations);
   auto s = array[0];
 
   printf("%lld %lld\n", s, array[iterations/2]);
