a6e38f682110aeaecfb13179d4d5c46c9f775ee7
angelomatni <angelomatni340@gmail.com>
2020-06-28 15:37:02 -0600

    LoopCarriedVariable: do not flag last live out as reducible
    Require at least one non-propagating evolution update

--- tests/unit/sccdag_attributes/suite/last_value_liveout/test.cpp
+++ tests/unit/sccdag_attributes/suite/last_value_liveout/test.cpp
@@ -1,13 +1,17 @@
 #include <stdio.h>
 
 int main (int argc, char *argv[]){
-  int v1 = 0, v2 = 0;
+  int v1 = 0, v2 = 0, v3 = 0;
 
   for (int i = 0; i < 100; ++i) {
     v1 = v1 + argc;
     v2 = v1 + 5;
+
+    if (i < argc) {
+      v3 = v1 - argc;
+    }
   }
 
-  printf("%d, %d\n", v1, v2);
+  printf("%d, %d, %d\n", v1, v2, v3);
   return 0;
 }
