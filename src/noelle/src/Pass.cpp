8640f7c9209f6c3e6e37cf2fe8cab1b8042263a7
Simone Campanoni <simo.xan@gmail.com>
2020-06-28 18:27:33 -0500

    Bug fixed

--- src/noelle/src/Pass.cpp
+++ src/noelle/src/Pass.cpp
@@ -17,7 +17,7 @@ using namespace llvm;
 static cl::opt<int> Verbose("noelle-verbose", cl::ZeroOrMore, cl::Hidden, cl::desc("Verbose output (0: disabled, 1: minimal, 2: maximal)"));
 static cl::opt<int> MinimumHotness("noelle-min-hot", cl::ZeroOrMore, cl::Hidden, cl::desc("Minimum hotness of code to be parallelized"));
 static cl::opt<int> MaximumCores("noelle-max-cores", cl::ZeroOrMore, cl::Hidden, cl::desc("Maximum number of logical cores that Noelle can use"));
-2tatic cl::opt<bool> DisableDSWP("noelle-disable-dswp", cl::ZeroOrMore, cl::Hidden, cl::desc("Disable DSWP"));
+static cl::opt<bool> DisableDSWP("noelle-disable-dswp", cl::ZeroOrMore, cl::Hidden, cl::desc("Disable DSWP"));
 static cl::opt<bool> DisableHELIX("noelle-disable-helix", cl::ZeroOrMore, cl::Hidden, cl::desc("Disable HELIX"));
 static cl::opt<bool> DisableDOALL("noelle-disable-doall", cl::ZeroOrMore, cl::Hidden, cl::desc("Disable DOALL"));
 static cl::opt<bool> DisableInliner("noelle-disable-inliner", cl::ZeroOrMore, cl::Hidden, cl::desc("Disable the function inliner"));
