23d6cf2dd281e42c7d29608cf5db3de3869e8748
Simone Campanoni <simo.xan@gmail.com>
2020-07-15 09:39:44 -0500

    Users: added a simple user of the framework

---
+++ users/tests/0/test.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main (){
+  printf("Start\n");
+
+  int *p = (int *)malloc(sizeof(int));
+  *p = 0;
+
+  for (int i=0; i < 10; i++){
+    *p = 1;
+    printf("%d\n", i);
+  }
+
+  printf("  Result = %d\n", *p);
+
+  printf("End\n");
+  return 0;
+}
