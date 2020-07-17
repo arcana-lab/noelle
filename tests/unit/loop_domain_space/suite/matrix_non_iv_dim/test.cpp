5cb35c526248e9851110274a02fec40e2eb58d90
angelomatni <angelomatni340@gmail.com>
2020-07-16 22:06:29 -0600

    Unit tests: LoopIterationDomainSpaceAnalysis matrix cast/shift tests

---
+++ tests/unit/loop_domain_space/suite/matrix_non_iv_dim/test.cpp
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <math.h>
+
+using namespace std;
+
+int main (int argc, char *argv[]){
+  if (argc < 1) return 0;
+
+  int64_t iterations = 10 * std::fmax(argc, 3);
+  int64_t iterations2 = (iterations / 2);
+  int * matrix = (int *)calloc(iterations * iterations2, sizeof(int));
+
+  for (int64_t i = 0; i < iterations; ++i) {
+
+    /*
+     * Constant index is understood
+     */
+    matrix[i * iterations2] = i * (i + iterations);
+    matrix[i * iterations2 + 3] = i * (i + iterations);
+
+    for (int64_t j = 5; j < iterations2; ++j) {
+
+      /*
+       * Function on sub-loop index is "understood"
+       * NOTE: Program correctness is assumed, that this index does not overflow
+       */
+      int boundedI = (int)std::fmin(i, iterations2 - 1);
+      matrix[i * iterations2 + boundedI] = i * (i + iterations);
+    }
+  }
+
+  printf("%d, %d\n", matrix[0], matrix[iterations * (iterations2 / 2)]);
+
+  return 0;
+}
