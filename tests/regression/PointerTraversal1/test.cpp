1f9b9d108fd01d3b8cb71eed6d3ed90f96e7330a
angelomatni <angelomatni340@gmail.com>
2020-08-14 20:20:15 -0600

    PointerTraversal regression tests

--- tests/regression/PointerTraversal1/test.cpp
+++ tests/regression/PointerTraversal1/test.cpp
@@ -6,6 +6,7 @@
 struct _NODE {
   int i1;
   int i2;
+  int i3;
 };
 typedef struct _NODE NODE;
 
@@ -16,18 +17,19 @@ int main (int argc, char *argv[]){
   auto nodePtr = nodes;
 
   auto nodePtrInt = (int64_t)nodePtr;
-  for (auto i = 0; i < NUM_NODES; ++i) {
-    nodePtrInt += 8;
+  for (auto i = 0; i < NUM_NODES - 1; ++i) {
+    nodePtrInt += 12;
     auto nodePtrPtr = (NODE *)nodePtrInt;
     nodePtrPtr->i1 = i;
     nodePtrPtr->i2 = i + argc;
+    nodePtrPtr->i3 = 1;
   }
 
   nodePtrInt = (int64_t)nodePtr;
-  for (auto i = 0; i < NUM_NODES; ++i) {
-    nodePtrInt += 8;
+  for (auto i = 0; i < NUM_NODES - 1; ++i) {
+    nodePtrInt += 12;
     auto nodePtrPtr = (NODE *)nodePtrInt;
-    printf("%d, %d\n", nodePtrPtr->i1, nodePtrPtr->i2);
+    printf("%d, %d, %d\n", nodePtrPtr->i1, nodePtrPtr->i2, nodePtrPtr->i3);
   }
 
   return 0;
