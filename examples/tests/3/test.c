b7229e12a63c7432031d7627d79832a058428f2c
Simone Campanoni <simo.xan@gmail.com>
2020-09-20 09:53:18 -0500

    Examples: improved the pass and tests

---
+++ examples/tests/3/test.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+void myG1 (void){
+  printf("Inside myG1\n");
+}
+
+void myG2 (void){
+  printf("Inside myG2\n");
+}
+
+void myG3 (void){
+  printf("Inside myG3\n");
+}
+
+void myF (void (*funcToInvoke) (void)){
+  (*funcToInvoke)();
+}
+
+int main (int argc, char *argv[]){
+  printf("Start\n");
+
+  if (argc > 2){
+    myF(myG1);
+  } {
+    myF(myG2);
+  }
+  printf("End\n");
+  return 0;
+}
