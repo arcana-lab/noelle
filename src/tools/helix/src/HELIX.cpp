69c6ab4674b04293ea9100d696c2cdb727e442d2
Simone Campanoni <simo.xan@gmail.com>
2020-12-10 09:22:15 -0600

    Loops: removed an API that didn't belong to LoopDependenceInfo

--- src/tools/helix/src/HELIX.cpp
+++ src/tools/helix/src/HELIX.cpp
@@ -268,7 +268,7 @@ void HELIX::createParallelizableTask (
    * Add the memory location of the environment used to store the exit block taken to leave the parallelized loop.
    * This location exists only if there is more than one loop exit.
    */
-  if (LDI->numberOfExits() > 1){ 
+  if (loopSummary->getLoopExitBasicBlocks().size() > 1){ 
     nonReducableVars.insert(LDI->environment->indexOfExitBlock());
   }
 
