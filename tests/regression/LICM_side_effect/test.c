fd0c6172338c4ed550dc0aa77e6636e8b1f10dce
Simone Campanoni <simo.xan@gmail.com>
2021-02-10 11:07:27 -0600

    Tests: simplified the new test

--- tests/regression/LICM_side_effect/test.c
+++ tests/regression/LICM_side_effect/test.c
@@ -3,43 +3,11 @@
 
 
 void myF(int num, int* ctr, char * class, double *xcr) {
-  double xcrref[5], xcrdif[5];
   *ctr = 1;
-
-  if (num == 12) {
-    *class = 'S';
-    xcrref[0] = 1.7034283709541311e-01;
-    xcrref[1] = 1.2975252070034097e-02;
-    xcrref[2] = 3.2527926989486055e-02;
-    xcrref[3] = 2.6436421275166801e-02;
-    xcrref[4] = 1.9211784131744430e-01;
-  } else if (num == 16) {
-    *class = 'B';
-      xcrref[0] = 1.4233597229287254e+03;
-      xcrref[1] = 9.9330522590150238e+01;
-      xcrref[2] = 3.5646025644535285e+02;
-      xcrref[3] = 3.2485447959084092e+02;
-      xcrref[4] = 3.2707541254659363e+03;
-  } else {
-    *ctr = 0;
-  }
-
-  for (int m = 0; m < 5; m++) {
-    xcrdif[m] = ((xcr[m]-xcrref[m])/xcrref[m]);
-  }
-
-  double epsilon = 1.0e-08;
-
-  if (*class != 'U') {
-    printf(" Comparison of RMS-norms of solution error\n");
-  } else {
-    printf(" RMS-norms of solution error\n");
-  }
-
   for (int i = 0; i < 5; i++) {
     if (*class == 'U') {
       printf("class == U\n");
-    } else if (xcrdif[i] > epsilon) {
+    } else if (xcr[i] <= 0.00000001F) {
       /* BUGGED, this store is being hoisted */
       *ctr = 0;
       printf("Over epsilon!\n");
@@ -76,4 +44,3 @@ int main(int argc, char *argv[]) {
   free(ctr);
   return 0;
 }
-
