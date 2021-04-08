2f3af2e582aab2d9daa8bd64e024c223002f22ea
Simone Campanoni <simo.xan@gmail.com>
2021-04-08 12:37:46 -0500

    Examples: Renamed  complex

--- examples/passes/callgraph/src/CatPass.cpp
+++ examples/passes/callgraph/src/CatPass.cpp
@@ -13,7 +13,6 @@ namespace {
 
   struct CAT : public ModulePass {
     static char ID; 
-    bool printDFA = false;
 
     CAT() : ModulePass(ID) {}
 
@@ -34,96 +33,6 @@ namespace {
        */
       auto fm = noelle.getFunctionsManager();
       auto mainF = fm->getEntryFunction();
-
-      /*
-       * Data flow analysis
-       */
-      auto dfa = noelle.getDataFlowAnalyses();
-      auto dfr = dfa.runReachableAnalysis(mainF);
-      errs() << "Data flow reachable analysis\n";
-      for (auto& inst : instructions(mainF)){
-        errs() << " Next are the instructions reachable from " << inst << "\n";
-        auto& outSet = dfr->OUT(&inst);
-        for (auto reachInst : outSet){
-          errs() << "   " << *reachInst << "\n";
-        }
-      }
-
-      /*
-       * New data flow analysis
-       *
-       * Fetch the hottest loop.
-       */
-      noelle.sortByHotness(*loops);
-      auto loop = (*loops)[0];
-      auto loopStructure = loop->getLoopStructure();
-      auto loopHeader = loopStructure->getHeader();
-      auto loopFunction = loopStructure->getFunction();
-
-      /*
-       * Fetch the data flow engine.
-       */
-      auto dfe = noelle.getDataFlowEngine();
-
-      /*
-       * Define the data flow equations
-       */
-      auto computeGEN = [loopStructure](Instruction *i, DataFlowResult *df) {
-        if (!loopStructure->isIncluded(i)){
-          return ;
-        }
-        auto& gen = df->GEN(i);
-        gen.insert(i);
-        return ;
-      };
-      auto computeKILL = [](Instruction *, DataFlowResult *) {
-        return ;
-      };
-      auto computeOUT = [loopHeader](std::set<Value *>& OUT, Instruction *succ, DataFlowResult *df) {
-
-        /*
-         * Check if the successor is the header.
-         * In this case, we do not propagate the reachable instructions.
-         * We do this because we are interested in understanding the reachability of instructions within a single iteration.
-         */
-        auto succBB = succ->getParent();
-        if (succ == &*loopHeader->begin()) {
-          return ;
-        }
-
-        /*
-         * Propagate the data flow values.
-         */
-        auto& inS = df->IN(succ);
-        OUT.insert(inS.begin(), inS.end());
-        return ;
-      } ;
-      auto computeIN = [](std::set<Value *>& IN, Instruction *inst, DataFlowResult *df) {
-        auto& genI = df->GEN(inst);
-        auto& outI = df->OUT(inst);
-        IN.insert(outI.begin(), outI.end());
-        IN.insert(genI.begin(), genI.end());
-        return ;
-      };
-
-      /*
-       * Run the data flow analysis
-       */
-      errs() << "Data flow reachable analysis within loop iteration\n";
-      auto customDfr = dfe.applyBackward(loopFunction, computeGEN, computeKILL, computeIN, computeOUT);
-
-      /*
-       * Print
-       */
-      for (auto bb : loopStructure->getBasicBlocks()){
-        for (auto &inst : *bb){
-          auto reachableInstsWithinIteration = customDfr->OUT(&inst);
-          errs() << " Next are the " << reachableInstsWithinIteration.size() << " loop instructions reachable from " << inst << "\n";
-          for (auto reachInst : reachableInstsWithinIteration){
-            errs() << "   " << *reachInst << "\n";
-          }
-        }
-      }
 
       /*
        * Call graph.
