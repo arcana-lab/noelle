f425a4c6b55aaef224e071c0be6392ba37ec9cab
Simone Campanoni <simo.xan@gmail.com>
2021-04-28 18:48:28 -0500

    PDG: fixed a memory corruption

--- src/core/pdg/src/PDGAnalysis_embedder.cpp
+++ src/core/pdg/src/PDGAnalysis_embedder.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016 - 2020  Angelo Matni, Yian Su, Simone Campanoni
+ * Copyright 2016 - 2021  Angelo Matni, Yian Su, Simone Campanoni
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 
@@ -87,7 +87,7 @@ void PDGAnalysis::embedNodesAsMetadata(PDG *pdg, LLVMContext &C, unordered_map<V
 }
 
 void PDGAnalysis::embedEdgesAsMetadata(PDG *pdg, LLVMContext &C, unordered_map<Value *, MDNode *> &nodeIDMap) {
-  unordered_map<Function *, vector<Metadata *>> functionEdgesMap;
+  std::unordered_map<Function *, vector<Metadata *>> functionEdgesMap;
 
   /*
    * Construct edge metadata
