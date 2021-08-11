3019d0945eb2d1b3497b2ac7a0d12f79dfc861d8
Simone Campanoni <simo.xan@gmail.com>
2021-08-11 08:39:56 -0500

    Enablers: minor

--- src/tools/enablers/src/Enablers.cpp
+++ src/tools/enablers/src/Enablers.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019 - 2020 Angelo Matni, Simone Campanoni
+ * Copyright 2019 - 2021 Angelo Matni, Simone Campanoni
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 
@@ -10,8 +10,7 @@
  */
 #include "EnablersManager.hpp"
 
-using namespace llvm;
-using namespace llvm::noelle;
+namespace llvm::noelle{
 
 bool EnablersManager::applyEnablers (
     LoopDependenceInfo *LDI,
@@ -300,3 +299,5 @@ bool EnablersManager::applyEnablers (
 
     return modified;
   }
+
+}
