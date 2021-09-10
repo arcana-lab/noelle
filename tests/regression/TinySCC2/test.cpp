91d812138a9f91bf80409168608f6fe133b730ac
Simone Campanoni <simo.xan@gmail.com>
2021-09-10 10:29:06 -0500

    Tests: added a new test

---
+++ tests/regression/TinySCC2/test.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main (int argc, char *argv[]){
+  if (argc < 1){
+    fprintf(stderr, "USAGE: %s LOOP_ITERATIONS\n", argv[0]);
+    return -1;
+  }
+  auto iterations = atoi(argv[1]);
+  if (iterations < 0) {
+    fprintf(stderr, "USAGE: %s requires a position iteration count\n", argv[0]);
+    return -1;
+  }
+
+  auto i = 100;
+  int accumValue = 3004340340;
+  for (auto j = 0; j < iterations * iterations; ++j) {
+    auto inc = 0;
+    accumValue += sqrt(i + j + inc++);
+    accumValue -= sqrt(i + j + inc++);
+  }
+  printf("%d\n", accumValue);
+  return 0;
+}
