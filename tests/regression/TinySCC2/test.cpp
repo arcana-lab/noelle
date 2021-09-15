dd583653b28c033f419f1e3feb23995876190784
Simone Campanoni <simo.xan@gmail.com>
2021-09-15 18:58:18 -0500

    Tests: simplified the new test

--- tests/regression/TinySCC2/test.cpp
+++ tests/regression/TinySCC2/test.cpp
@@ -16,15 +16,6 @@ int main (int argc, char *argv[]){
 
   int accumValue = sqrt(i);
   for (auto j = 0; j < iterations * iterations; ++j) {
-    auto inc = 0;
-    accumValue += sqrt(j);
-    accumValue -= sqrt(j);
-    accumValue += sqrt(j);
-    accumValue -= sqrt(j);
-    accumValue += sqrt(j);
-    accumValue -= sqrt(j);
-    accumValue += sqrt(j);
-    accumValue -= sqrt(j);
     accumValue += sqrt(j);
     accumValue -= sqrt(j);
   }
