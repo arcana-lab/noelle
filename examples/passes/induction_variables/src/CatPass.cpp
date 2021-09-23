547cfeb5bf1fc0c080ebc8de050bef69a78d57ca
Simone Campanoni <simo.xan@gmail.com>
2021-09-23 12:24:44 -0500

    Examples: fixed the IV example

--- examples/passes/induction_variables/src/CatPass.cpp
+++ examples/passes/induction_variables/src/CatPass.cpp
@@ -27,12 +27,6 @@ namespace {
       auto& noelle = getAnalysis<Noelle>();
 
       /*
-       * fetch the loops with all their abstractions 
-       * (e.g., loop dependence graph, sccdag)
-       */
-      auto loops = noelle.getLoops();
- 
-      /*
        * Check if we have profiles.
        */
       auto hot = noelle.getProfiles();
@@ -42,26 +36,36 @@ namespace {
       }
 
       /*
+       * Fetch the loops with all their abstractions 
+       * (e.g., loop dependence graph, sccdag)
+       */
+      auto loops = noelle.getLoopStructures();
+      if (loops->size() == 0){
+        errs() << "The program has no loops\n";
+        return false;
+      }
+ 
+      /*
        * Fetch the hottest loop.
        */
       noelle.sortByHotness(*loops);
-      auto hottestLoop = loops->front();
-      auto LS = hottestLoop->getLoopStructure();
-      auto entryInst = LS->getEntryInstruction();
+      auto hottestLoopLS = loops->front();
+      auto hottestLoop = noelle.getLoop(hottestLoopLS);
+      auto entryInst = hottestLoopLS->getEntryInstruction();
 
       /*
        * Print some information about the loop.
        */
       errs() << "Hottest loop " << *entryInst << "\n";
-      errs() << " Function = " << LS->getFunction()->getName() << "\n";
-      errs() << " Nesting level = " << LS->getNestingLevel() << "\n";
-      errs() << " This loop has " << LS->getNumberOfSubLoops() << " sub-loops (including sub-loops of sub-loops)\n";
+      errs() << " Function = " << hottestLoopLS->getFunction()->getName() << "\n";
+      errs() << " Nesting level = " << hottestLoopLS->getNestingLevel() << "\n";
+      errs() << " This loop has " << hottestLoopLS->getNumberOfSubLoops() << " sub-loops (including sub-loops of sub-loops)\n";
 
       /*
        * Fetch the governing IV
        */
       auto IVM = hottestLoop->getInductionVariableManager();
-      auto GIV_attr = IVM->getLoopGoverningIVAttribution(*LS);
+      auto GIV_attr = IVM->getLoopGoverningIVAttribution(*hottestLoopLS);
       if (  false
             || (GIV_attr == nullptr)
             ){
@@ -75,7 +79,7 @@ namespace {
        */
       auto cond = GIV_attr->getHeaderCmpInst();
       auto IV = GIV_attr->getInductionVariable();
-      auto condValue = GIV_attr->getHeaderCmpInstConditionValue();
+      auto condValue = GIV_attr->getHeaderCmpInst();
       auto startValue = IV.getStartValue();
       errs() << " Governing induction variable\n";
       errs() << "   Condition = " << *cond << "\n";
@@ -89,12 +93,12 @@ namespace {
       /*
        * Fetch the loop governing IV utility
        */
-      LoopGoverningIVUtility GIVUtility(*GIV_attr);
+      LoopGoverningIVUtility GIVUtility(hottestLoopLS, *IVM, *GIV_attr);
 
       /*
        * Generate the code to compute the total number of iterations for the current loop invocation.
        */
-      IRBuilder<> builder(LS->getPreHeader()->getTerminator());
+      IRBuilder<> builder(hottestLoopLS->getPreHeader()->getTerminator());
       auto loopIterations = GIVUtility.generateCodeToComputeTheTripCount(builder);
       errs() << "   Trip count = " << *loopIterations << "\n";
 
