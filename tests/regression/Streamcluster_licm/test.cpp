3efd5d0bf96be774b586c4fc17ba8ec9d0279f88
Yian Su <yiansu2018@u.northwestern.edu>
2021-10-11 13:33:19 -0500

    Test: modify Streamcluster_licm regression test to have determined output

--- tests/regression/Streamcluster_licm/test.cpp
+++ tests/regression/Streamcluster_licm/test.cpp
@@ -1,8 +1,10 @@
+#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
 
 struct Point {
-  float weight; // the existence of this field caused a mem dependence removed by SCAF and leads to a seg fault due to loading from uninitialized memory
+  float weight; // the existence of this field triggers a bug inside SCAF and causes a mem dependece to be removed, which finally leads to a seg fault due to loading from uninitialized memory
   float *coord;
 };
 
@@ -13,13 +15,14 @@ struct Points {
 void run(int num, int dim, Points *points) {
   float cost = 0.0;
   for (int i = 0; i < num; i++) {
-    Point p = points->p[i];
+    Point p0 = points->p[0];
+    Point p1 = points->p[i];
     for (int j = 0; j < dim; j++) {
-      cost += p.coord[j];
+      cost += p0.coord[i] - p1.coord[j];
     }
   }
 
-  fprintf(stderr, "%f\n", cost);
+  fprintf(stdout, "%d\n", (int)cost);
 }
 
 int main(int argc, char **argv) {
@@ -29,8 +32,14 @@ int main(int argc, char **argv) {
   Points points;
   points.p = (Point *)malloc(num * sizeof(Point));
 
+  float *coordValues = (float *)malloc(dim * sizeof(float)); // all point have the same coord, this ensures a determined program output
+  for (int i = 0; i < dim; i++) {
+    coordValues[i] = lrand48() / (float)INT_MAX;
+  }
+
   float *coords = (float *)malloc(num * dim * sizeof(float));
   for (int i = 0; i < num; i++) {
+    memcpy(&coords[i * dim], coordValues, dim * sizeof(float));
     points.p[i].coord = &coords[i * dim];
     points.p[i].weight = 1.0;
   }
