86549a240a70ba95dd8c0b5f1e02fa4e723bb32b
Simone Campanoni <simo.xan@gmail.com>
2021-11-08 19:02:16 -0600

    Tests: simplified a test

--- tests/regression/InductionVariable2/test.cpp
+++ tests/regression/InductionVariable2/test.cpp
@@ -11,10 +11,6 @@ void computeSum (long long int *a, long long int iters, long long int seed, long
       z++;
       a[i] /= z;
       z++;
-      a[i] /= z;
-      z++;
-      a[i] /= z;
-      z++;
       a[i] += z;
     }
     i++;
