5259fbcdf9227beed78a9bf4d34ed8c8ca2542b3
Simone Campanoni <simo.xan@gmail.com>
2021-11-09 18:41:18 -0600

    LoopSize: new tool to improve the fixed-point tool

---
+++ src/tools/loopsize/src/LoopSize.cpp
@@ -0,0 +1,89 @@
+/*
+ * Copyright 2021 Simone Campanoni
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. 
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+#pragma once
+
+#include "noelle/core/Noelle.hpp"
+#include "noelle/tools/LoopSize.hpp"
+
+namespace llvm::noelle {
+
+LoopSize::LoopSize() 
+  : ModulePass{ID}
+{
+}
+
+bool LoopSize::runOnModule (Module &M) {
+
+  /*
+   * Fetch NOELLE
+   */
+  auto& noelle = getAnalysis<Noelle>();
+
+  /*
+   * Fetch all program loops.
+   */
+  auto loops = noelle.getLoopStructures();
+
+  /*
+   * Compute the code size.
+   */
+  uint64_t s = 0;
+  for (auto &F : M){
+    for (auto &bb : F){
+      for (auto &I : bb){
+
+        /*
+         * Check the current instruction
+         */
+        if (isa<PHINode>(&I)){
+          continue ;
+        }
+        if (auto brInst = dyn_cast<BranchInst>(&I)){
+          if (brInst->isUnconditional()){
+            continue ;
+          }
+        }
+
+        /*
+         * Fetch the innermost loop that contains the instruction.
+         */
+        auto loop = noelle.getInnermostLoopThatContains(*loops, &I);
+        if (loop == nullptr){
+
+          /*
+           * The instruction isn't in any loop
+           */
+          continue ;
+        }
+
+        /*
+         * Fetch the nesting level.
+         */
+        auto nl = loop->getNestingLevel();
+
+        /*
+         * Compute the instruction cost.
+         */
+        auto iCost = nl;
+
+        /*
+         * Update the total cost
+         */
+        s += iCost;
+      }
+    }
+  }
+  outs() << s << "\n";
+
+  return false; 
+}
+
+}
