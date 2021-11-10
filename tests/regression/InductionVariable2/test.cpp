a1618a4b65c678043464551b84ed3f0c283f27b5
Simone Campanoni <simo.xan@gmail.com>
2021-11-10 13:19:15 -0600

    Tests: simplified a test

--- tests/regression/InductionVariable2/test.cpp
+++ tests/regression/InductionVariable2/test.cpp
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <math.h>
 
-void computeSum (long long int *a, long long int iters, long long int seed, long long int innerIters){
+void computeSum (long long int *a, long long int iters, long long int innerIters){
   long long int z = (rand() + 1) %20;
   for (long long int i=0; i < iters; i++){
     for (auto k=0; k < innerIters; k++){
@@ -18,8 +18,8 @@ int main (int argc, char *argv[]){
   /*
    * Check the inputs.
    */
-  if (argc < 4){
-    fprintf(stderr, "USAGE: %s LOOP_ITERATIONS SEED INNER_ITERS\n", argv[0]);
+  if (argc < 3){
+    fprintf(stderr, "USAGE: %s LOOP_ITERATIONS INNER_ITERS\n", argv[0]);
     return -1;
   }
   auto iterations = atoll(argv[1]);
@@ -27,11 +27,10 @@ int main (int argc, char *argv[]){
     iterations++;
   }
   iterations *= 100;
-  auto seed = atoll(argv[2]);
-  auto innerIters = atoll(argv[3]);
+  auto innerIters = atoll(argv[2]);
 
   long long int *array = (long long int *) calloc(iterations, sizeof(long long int));
-  computeSum(array, iterations, seed, innerIters);
+  computeSum(array, iterations, innerIters);
   printf("%lld\n", array[iterations/2]);
 
   return 0;
