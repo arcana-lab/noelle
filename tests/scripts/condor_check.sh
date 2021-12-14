fbee036f7cfef1ba6847a92b1b94faa57c0ea2d0
Simone Campanoni <simo.xan@gmail.com>
2021-12-14 13:29:27 -0600

    Tests: performance tests now register a slowdown only for speedup losts higher than 0.3

--- tests/scripts/condor_check.sh
+++ tests/scripts/condor_check.sh
@@ -161,7 +161,7 @@ if test -f performance/speedups.txt ; then
     paste $tempSpeedups $tempOracle > $tempCompare ;
     awk '
       {
-        if (  ($2 < ($4 * 0.9)) && (($4 - $2) > 0.2)   ){
+        if (  ($2 < ($4 * 0.9)) && (($4 - $2) > 0.3)   ){
           printf("    Performance degradation for %s (from %.1fx to %.1fx)\n", $1, $4, $2);
         }
       }' $tempCompare > $tempOutput ;
