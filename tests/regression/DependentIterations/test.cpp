33a1fd3ff08deca5de459f53c6b4b8d1c99dc24e
Simone Campanoni <simo.xan@gmail.com>
2022-03-30 14:30:43 -0500

    Tests: fixed a test

--- tests/regression/DependentIterations/test.cpp
+++ tests/regression/DependentIterations/test.cpp
@@ -16,16 +16,15 @@ int main (int argc, char *argv[]){
 
   auto array = (long long int *) malloc(sizeof(long long int) * iterations);
 
-  array[0] = 42;
+  for (auto i=0; i < iterations; i++){
+    auto index = (i % (argc - 1)) + 1;
+    array[i] = atoll(argv[index]);
+  }
   for (auto i=1; i < iterations; i++){
     array[i] = array[i-1] * i;
   }
 
-  int64_t s=0;
-  for (auto i=0; i < iterations; i++){
-    s += array[i];
-  }
-
+  int64_t s = array[0] + array[iterations - 1] + array[iterations / 2];
   printf("%lld\n", s);
 
   return 0;
