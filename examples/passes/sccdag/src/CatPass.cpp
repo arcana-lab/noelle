bad01a7dfc47b93c75b0def30f391c89ed63e732
Simone Campanoni <simo.xan@gmail.com>
2022-04-07 15:13:49 -0500

    Examples: added another example

---
+++ examples/passes/sccdag/src/CatPass.cpp
@@ -0,0 +1,67 @@
+#include "llvm/Pass.h"
+#include "llvm/IR/Function.h"
+#include "llvm/Support/raw_ostream.h"
+#include "llvm/IR/LegacyPassManager.h"
+#include "llvm/Transforms/IPO/PassManagerBuilder.h"
+#include <algorithm>
+
+#include "noelle/core/Noelle.hpp"
+
+using namespace llvm::noelle ;
+
+namespace {
+
+  struct CAT : public ModulePass {
+    static char ID; 
+
+    CAT() : ModulePass(ID) {}
+
+    bool doInitialization (Module &M) override {
+      return false;
+    }
+
+    bool runOnModule (Module &M) override {
+
+      /*
+       * Fetch NOELLE
+       */
+      auto& noelle = getAnalysis<Noelle>();
+
+      /*
+       * Fetch the PDG
+       */
+      auto PDG = noelle.getProgramDependenceGraph();
+
+      /*
+       * Fetch the FDG of "main"
+       */
+      auto fm = noelle.getFunctionsManager();
+      auto mainF = fm->getEntryFunction();
+      auto FDG = noelle.getFunctionDependenceGraph(mainF);
+
+      /*
+       * Compute the SCCDAG of the FDG of "main"
+       */
+      auto mainSCCDAG = new SCCDAG(FDG);
+
+      return false;
+    }
+
+    void getAnalysisUsage(AnalysisUsage &AU) const override {
+      AU.addRequired<Noelle>();
+    }
+  };
+}
+
+// Next there is code to register your pass to "opt"
+char CAT::ID = 0;
+static RegisterPass<CAT> X("CAT", "Simple user of the Noelle framework");
+
+// Next there is code to register your pass to "clang"
+static CAT * _PassMaker = NULL;
+static RegisterStandardPasses _RegPass1(PassManagerBuilder::EP_OptimizerLast,
+    [](const PassManagerBuilder&, legacy::PassManagerBase& PM) {
+        if(!_PassMaker){ PM.add(_PassMaker = new CAT());}}); // ** for -Ox
+static RegisterStandardPasses _RegPass2(PassManagerBuilder::EP_EnabledOnOptLevel0,
+    [](const PassManagerBuilder&, legacy::PassManagerBase& PM) {
+        if(!_PassMaker){ PM.add(_PassMaker = new CAT()); }}); // ** for -O0
