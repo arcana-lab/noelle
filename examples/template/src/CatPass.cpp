7145742a471bed0b956cd12e81ca3e7326d727d8
Tommy McMichen <mcmichen@u.northwestern.edu>
2022-06-24 17:01:20 -0500

    Formatted examples/template

--- examples/template/src/CatPass.cpp
+++ examples/template/src/CatPass.cpp
@@ -1,51 +1,59 @@
-#include "llvm/Pass.h"
 #include "llvm/IR/Function.h"
-#include "llvm/Support/raw_ostream.h"
 #include "llvm/IR/LegacyPassManager.h"
+#include "llvm/Pass.h"
+#include "llvm/Support/raw_ostream.h"
 #include "llvm/Transforms/IPO/PassManagerBuilder.h"
 
 #include "Noelle.hpp"
 
-using namespace llvm::noelle ;
+using namespace llvm::noelle;
 
 namespace {
 
-  struct CAT : public ModulePass {
-    static char ID; 
+struct CAT : public ModulePass {
+  static char ID;
 
-    CAT() : ModulePass(ID) {}
+  CAT() : ModulePass(ID) {}
 
-    bool doInitialization (Module &M) override {
-      return false;
-    }
+  bool doInitialization(Module &M) override {
+    return false;
+  }
 
-    bool runOnModule (Module &M) override {
-      auto modified = true;
+  bool runOnModule(Module &M) override {
+    auto modified = true;
 
-      /*
-       * Fetch NOELLE
-       */
-      auto& noelle = getAnalysis<Noelle>();
+    /*
+     * Fetch NOELLE
+     */
+    auto &noelle = getAnalysis<Noelle>();
 
-      return modified;
-    }
+    return modified;
+  }
 
-    void getAnalysisUsage(AnalysisUsage &AU) const override {
-      AU.addRequired<Noelle>();
-    }
-  };
+  void getAnalysisUsage(AnalysisUsage &AU) const override {
+    AU.addRequired<Noelle>();
+  }
+};
 
-}
+} // namespace
 
 // Next there is code to register your pass to "opt"
 char CAT::ID = 0;
 static RegisterPass<CAT> X("CAT", "Simple user of the Noelle framework");
 
 // Next there is code to register your pass to "clang"
-static CAT * _PassMaker = NULL;
+static CAT *_PassMaker = NULL;
 static RegisterStandardPasses _RegPass1(PassManagerBuilder::EP_OptimizerLast,
-    [](const PassManagerBuilder&, legacy::PassManagerBase& PM) {
-        if(!_PassMaker){ PM.add(_PassMaker = new CAT());}}); // ** for -Ox
-static RegisterStandardPasses _RegPass2(PassManagerBuilder::EP_EnabledOnOptLevel0,
-    [](const PassManagerBuilder&, legacy::PassManagerBase& PM) {
-        if(!_PassMaker){ PM.add(_PassMaker = new CAT()); }}); // ** for -O0
+                                        [](const PassManagerBuilder &,
+                                           legacy::PassManagerBase &PM) {
+                                          if (!_PassMaker) {
+                                            PM.add(_PassMaker = new CAT());
+                                          }
+                                        }); // ** for -Ox
+static RegisterStandardPasses _RegPass2(
+    PassManagerBuilder::EP_EnabledOnOptLevel0,
+    [](const PassManagerBuilder &, legacy::PassManagerBase &PM) {
+      if (!_PassMaker) {
+        PM.add(_PassMaker = new CAT());
+      }
+    }); // ** for -O0
