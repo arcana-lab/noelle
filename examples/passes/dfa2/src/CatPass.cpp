0d3d3e6dafc440d9552d0801e1f787f142fa6158
Tommy McMichen <mcmichen@u.northwestern.edu>
2022-06-24 17:00:21 -0500

    Formatted examples/passes

--- examples/passes/dfa2/src/CatPass.cpp
+++ examples/passes/dfa2/src/CatPass.cpp
@@ -1,71 +1,80 @@
-#include "llvm/Pass.h"
 #include "llvm/IR/Function.h"
-#include "llvm/Support/raw_ostream.h"
 #include "llvm/IR/LegacyPassManager.h"
+#include "llvm/Pass.h"
+#include "llvm/Support/raw_ostream.h"
 #include "llvm/Transforms/IPO/PassManagerBuilder.h"
 #include <algorithm>
 
 #include "noelle/core/Noelle.hpp"
 
-using namespace llvm::noelle ;
+using namespace llvm::noelle;
 
 namespace {
 
-  struct CAT : public ModulePass {
-    static char ID; 
+struct CAT : public ModulePass {
+  static char ID;
 
-    CAT() : ModulePass(ID) {}
+  CAT() : ModulePass(ID) {}
 
-    bool doInitialization (Module &M) override {
-      return false;
-    }
+  bool doInitialization(Module &M) override {
+    return false;
+  }
 
-    bool runOnModule (Module &M) override {
+  bool runOnModule(Module &M) override {
 
-      /*
-       * Fetch NOELLE
-       */
-      auto& noelle = getAnalysis<Noelle>();
-      errs() << "The program has " << noelle.numberOfProgramInstructions() << " instructions\n";
+    /*
+     * Fetch NOELLE
+     */
+    auto &noelle = getAnalysis<Noelle>();
+    errs() << "The program has " << noelle.numberOfProgramInstructions()
+           << " instructions\n";
 
-      /*
-       * Fetch the entry point.
-       */
-      auto fm = noelle.getFunctionsManager();
-      auto mainF = fm->getEntryFunction();
+    /*
+     * Fetch the entry point.
+     */
+    auto fm = noelle.getFunctionsManager();
+    auto mainF = fm->getEntryFunction();
 
-      /*
-       * Data flow analyses
-       */
-      auto dfa = noelle.getDataFlowAnalyses();
-      auto dfr = dfa.runReachableAnalysis(mainF);
-      errs() << "Data flow reachable analysis\n";
-      for (auto& inst : instructions(mainF)){
-        errs() << " Next are the instructions reachable from " << inst << "\n";
-        auto& outSet = dfr->OUT(&inst);
-        for (auto reachInst : outSet){
-          errs() << "   " << *reachInst << "\n";
-        }
+    /*
+     * Data flow analyses
+     */
+    auto dfa = noelle.getDataFlowAnalyses();
+    auto dfr = dfa.runReachableAnalysis(mainF);
+    errs() << "Data flow reachable analysis\n";
+    for (auto &inst : instructions(mainF)) {
+      errs() << " Next are the instructions reachable from " << inst << "\n";
+      auto &outSet = dfr->OUT(&inst);
+      for (auto reachInst : outSet) {
+        errs() << "   " << *reachInst << "\n";
       }
-
-      return false;
     }
 
-    void getAnalysisUsage(AnalysisUsage &AU) const override {
-      AU.addRequired<Noelle>();
-    }
-  };
-}
+    return false;
+  }
+
+  void getAnalysisUsage(AnalysisUsage &AU) const override {
+    AU.addRequired<Noelle>();
+  }
+};
+} // namespace
 
 // Next there is code to register your pass to "opt"
 char CAT::ID = 0;
 static RegisterPass<CAT> X("CAT", "Simple user of the Noelle framework");
 
 // Next there is code to register your pass to "clang"
-static CAT * _PassMaker = NULL;
+static CAT *_PassMaker = NULL;
 static RegisterStandardPasses _RegPass1(PassManagerBuilder::EP_OptimizerLast,
-    [](const PassManagerBuilder&, legacy::PassManagerBase& PM) {
-        if(!_PassMaker){ PM.add(_PassMaker = new CAT());}}); // ** for -Ox
-static RegisterStandardPasses _RegPass2(PassManagerBuilder::EP_EnabledOnOptLevel0,
-    [](const PassManagerBuilder&, legacy::PassManagerBase& PM) {
-        if(!_PassMaker){ PM.add(_PassMaker = new CAT()); }}); // ** for -O0
+                                        [](const PassManagerBuilder &,
+                                           legacy::PassManagerBase &PM) {
+                                          if (!_PassMaker) {
+                                            PM.add(_PassMaker = new CAT());
+                                          }
+                                        }); // ** for -Ox
+static RegisterStandardPasses _RegPass2(
+    PassManagerBuilder::EP_EnabledOnOptLevel0,
+    [](const PassManagerBuilder &, legacy::PassManagerBase &PM) {
+      if (!_PassMaker) {
+        PM.add(_PassMaker = new CAT());
+      }
+    }); // ** for -O0
--- examples/passes/simple/src/CatPass.cpp
+++ examples/passes/simple/src/CatPass.cpp
@@ -1,56 +1,64 @@
-#include "llvm/Pass.h"
 #include "llvm/IR/Function.h"
-#include "llvm/Support/raw_ostream.h"
 #include "llvm/IR/LegacyPassManager.h"
+#include "llvm/Pass.h"
+#include "llvm/Support/raw_ostream.h"
 #include "llvm/Transforms/IPO/PassManagerBuilder.h"
 
 #include "noelle/core/Noelle.hpp"
 
-using namespace llvm::noelle ;
+using namespace llvm::noelle;
 
 namespace {
 
-  struct CAT : public ModulePass {
-    static char ID; 
+struct CAT : public ModulePass {
+  static char ID;
 
-    CAT() : ModulePass(ID) {}
+  CAT() : ModulePass(ID) {}
 
-    bool doInitialization (Module &M) override {
-      return false;
-    }
+  bool doInitialization(Module &M) override {
+    return false;
+  }
 
-    bool runOnModule (Module &M) override {
+  bool runOnModule(Module &M) override {
 
-      /*
-       * Fetch NOELLE
-       */
-      auto& noelle = getAnalysis<Noelle>();
+    /*
+     * Fetch NOELLE
+     */
+    auto &noelle = getAnalysis<Noelle>();
 
-      /*
-       * Use NOELLE
-       */
-      auto insts = noelle.numberOfProgramInstructions();
-      errs() << "The program has " << insts << " instructions\n";
+    /*
+     * Use NOELLE
+     */
+    auto insts = noelle.numberOfProgramInstructions();
+    errs() << "The program has " << insts << " instructions\n";
 
-      return false;
-    }
+    return false;
+  }
 
-    void getAnalysisUsage(AnalysisUsage &AU) const override {
-      AU.addRequired<Noelle>();
-    }
-  };
+  void getAnalysisUsage(AnalysisUsage &AU) const override {
+    AU.addRequired<Noelle>();
+  }
+};
 
-}
+} // namespace
 
 // Next there is code to register your pass to "opt"
 char CAT::ID = 0;
 static RegisterPass<CAT> X("CAT", "Simple user of the Noelle framework");
 
 // Next there is code to register your pass to "clang"
-static CAT * _PassMaker = NULL;
+static CAT *_PassMaker = NULL;
 static RegisterStandardPasses _RegPass1(PassManagerBuilder::EP_OptimizerLast,
-    [](const PassManagerBuilder&, legacy::PassManagerBase& PM) {
-        if(!_PassMaker){ PM.add(_PassMaker = new CAT());}}); // ** for -Ox
-static RegisterStandardPasses _RegPass2(PassManagerBuilder::EP_EnabledOnOptLevel0,
-    [](const PassManagerBuilder&, legacy::PassManagerBase& PM) {
-        if(!_PassMaker){ PM.add(_PassMaker = new CAT()); }}); // ** for -O0
+                                        [](const PassManagerBuilder &,
+                                           legacy::PassManagerBase &PM) {
+                                          if (!_PassMaker) {
+                                            PM.add(_PassMaker = new CAT());
+                                          }
+                                        }); // ** for -Ox
+static RegisterStandardPasses _RegPass2(
+    PassManagerBuilder::EP_EnabledOnOptLevel0,
+    [](const PassManagerBuilder &, legacy::PassManagerBase &PM) {
+      if (!_PassMaker) {
+        PM.add(_PassMaker = new CAT());
+      }
+    }); // ** for -O0
