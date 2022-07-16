80aecca813dbd2e36c0f5c62febe46afaccdbe22
Tommy McMichen <mcmichen@u.northwestern.edu>
2022-07-16 12:30:49 -0500

    Added include guards to architecture

--- src/core/architecture/include/noelle/core/Architecture.hpp
+++ src/core/architecture/include/noelle/core/Architecture.hpp
@@ -19,6 +19,8 @@
  OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
  OR OTHER DEALINGS IN THE SOFTWARE.
  */
+#ifndef NOELLE_SRC_CORE_ARCHITECTURE_H
+#define NOELLE_SRC_CORE_ARCHITECTURE_H
 #pragma once
 
 #include "noelle/core/SystemHeaders.hpp"
@@ -39,3 +41,5 @@ private:
 };
 
 } // namespace llvm::noelle
+
+#endif
