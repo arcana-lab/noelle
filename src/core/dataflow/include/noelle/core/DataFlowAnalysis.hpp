1dc22b873b9d6ad199a9d98775bc64107799b64b
Tommy McMichen <mcmichen@u.northwestern.edu>
2022-07-16 12:29:32 -0500

    Added include guards to dataflow

--- src/core/dataflow/include/noelle/core/DataFlowAnalysis.hpp
+++ src/core/dataflow/include/noelle/core/DataFlowAnalysis.hpp
@@ -19,6 +19,8 @@
  OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
  OR OTHER DEALINGS IN THE SOFTWARE.
  */
+#ifndef NOELLE_SRC_CORE_DATAFLOWANALYSIS_H
+#define NOELLE_SRC_CORE_DATAFLOWANALYSIS_H
 #pragma once
 
 #include "noelle/core/SystemHeaders.hpp"
@@ -43,3 +45,5 @@ public:
 };
 
 } // namespace llvm::noelle
+
+#endif
