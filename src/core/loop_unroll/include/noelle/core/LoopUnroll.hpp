15f4c0d71af1b2ea9f51f4aac29652a5cac8a651
Tommy McMichen <mcmichen@u.northwestern.edu>
2022-07-16 12:27:16 -0500

    Added include guards to loop_unroll

--- src/core/loop_unroll/include/noelle/core/LoopUnroll.hpp
+++ src/core/loop_unroll/include/noelle/core/LoopUnroll.hpp
@@ -19,6 +19,8 @@
  OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
  OR OTHER DEALINGS IN THE SOFTWARE.
  */
+#ifndef NOELLE_SRC_CORE_LOOPUNROLL_H
+#define NOELLE_SRC_CORE_LOOPUNROLL_H
 #pragma once
 
 #include "noelle/core/SystemHeaders.hpp"
@@ -54,3 +56,5 @@ private:
 };
 
 } // namespace llvm::noelle
+
+#endif
