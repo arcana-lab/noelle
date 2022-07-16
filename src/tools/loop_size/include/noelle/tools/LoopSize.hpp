6be6fda7b4e8711a85cf60df72f302f2958a179c
Tommy McMichen <mcmichen@u.northwestern.edu>
2022-07-16 12:34:26 -0500

    Added include guards to loop_size

--- src/tools/loop_size/include/noelle/tools/LoopSize.hpp
+++ src/tools/loop_size/include/noelle/tools/LoopSize.hpp
@@ -19,6 +19,8 @@
  OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
  OR OTHER DEALINGS IN THE SOFTWARE.
  */
+#ifndef NOELLE_SRC_TOOLS_LOOPSIZE_H
+#define NOELLE_SRC_TOOLS_LOOPSIZE_H
 #pragma once
 
 #include "noelle/core/SystemHeaders.hpp"
@@ -44,3 +46,5 @@ private:
 };
 
 } // namespace llvm::noelle
+
+#endif
