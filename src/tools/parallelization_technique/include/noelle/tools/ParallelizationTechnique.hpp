779b6e52a1533bc962aa4195850aa6beb260bd9d
Tommy McMichen <mcmichen@u.northwestern.edu>
2022-07-16 12:33:39 -0500

    Added include guards to parallelization_technique

--- src/tools/parallelization_technique/include/noelle/tools/ParallelizationTechnique.hpp
+++ src/tools/parallelization_technique/include/noelle/tools/ParallelizationTechnique.hpp
@@ -19,6 +19,8 @@
  OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
  OR OTHER DEALINGS IN THE SOFTWARE.
  */
+#ifndef NOELLE_SRC_TOOLS_PARALLELIZATIONTECHNIQUE_H
+#define NOELLE_SRC_TOOLS_PARALLELIZATIONTECHNIQUE_H
 #pragma once
 
 #include "noelle/core/SystemHeaders.hpp"
@@ -207,3 +209,5 @@ protected:
 };
 
 } // namespace llvm::noelle
+
+#endif
