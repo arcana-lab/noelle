ced8fd33a64d1284008e3442b40e18e088cea85d
Tommy McMichen <mcmichen@u.northwestern.edu>
2022-07-16 12:28:19 -0500

    Added include guards to loop_environment

--- src/core/loop_environment/include/noelle/core/LoopEnvironmentUser.hpp
+++ src/core/loop_environment/include/noelle/core/LoopEnvironmentUser.hpp
@@ -19,6 +19,8 @@
  OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
  OR OTHER DEALINGS IN THE SOFTWARE.
  */
+#ifndef NOELLE_SRC_CORE_LOOPENVIRONMENTUSER_H
+#define NOELLE_SRC_CORE_LOOPENVIRONMENTUSER_H
 #pragma once
 
 #include "noelle/core/SystemHeaders.hpp"
@@ -65,3 +67,5 @@ private:
 };
 
 } // namespace llvm::noelle
+
+#endif
