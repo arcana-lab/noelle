fd81e2ffc0b18a2e5faf68c2611f8f3fa1423cff
Tommy McMichen <mcmichen@u.northwestern.edu>
2022-07-16 12:25:28 -0500

    Added include guards to task

--- src/core/task/include/noelle/core/Task.hpp
+++ src/core/task/include/noelle/core/Task.hpp
@@ -19,6 +19,8 @@
  OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
  OR OTHER DEALINGS IN THE SOFTWARE.
  */
+#ifndef NOELLE_SRC_CORE_TASK_H
+#define NOELLE_SRC_CORE_TASK_H
 #pragma once
 
 #include "noelle/core/SystemHeaders.hpp"
@@ -165,3 +167,5 @@ protected:
 };
 
 } // namespace llvm::noelle
+
+#endif
