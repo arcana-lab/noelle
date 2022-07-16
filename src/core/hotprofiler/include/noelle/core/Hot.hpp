932753843a82dc002da22fca2cec103f7efa2d7d
Tommy McMichen <mcmichen@u.northwestern.edu>
2022-07-16 12:29:08 -0500

    Added include guards to hotprofiler

--- src/core/hotprofiler/include/noelle/core/Hot.hpp
+++ src/core/hotprofiler/include/noelle/core/Hot.hpp
@@ -19,6 +19,8 @@
  OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
  OR OTHER DEALINGS IN THE SOFTWARE.
  */
+#ifndef NOELLE_SRC_CORE_HOT_H
+#define NOELLE_SRC_CORE_HOT_H
 #pragma once
 
 #include "noelle/core/SystemHeaders.hpp"
@@ -186,3 +188,5 @@ private:
 };
 
 } // namespace llvm::noelle
+
+#endif
