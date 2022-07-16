2c32062003c11be1c34a560a042af68599848f37
Tommy McMichen <mcmichen@u.northwestern.edu>
2022-07-16 12:28:45 -0500

    Added include guards to invariants

--- src/core/invariants/include/noelle/core/Invariants.hpp
+++ src/core/invariants/include/noelle/core/Invariants.hpp
@@ -19,6 +19,8 @@
  OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
  OR OTHER DEALINGS IN THE SOFTWARE.
  */
+#ifndef NOELLE_SRC_CORE_INVARIANTS_H
+#define NOELLE_SRC_CORE_INVARIANTS_H
 #pragma once
 
 #include "noelle/core/PDG.hpp"
@@ -81,3 +83,5 @@ private:
 };
 
 } // namespace llvm::noelle
+
+#endif
