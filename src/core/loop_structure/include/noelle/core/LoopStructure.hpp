6f80c1ae41736421e498391f5d23c381a4320f76
Tommy McMichen <mcmichen@u.northwestern.edu>
2022-07-16 12:27:43 -0500

    Added include guards to loop_structure

--- src/core/loop_structure/include/noelle/core/LoopStructure.hpp
+++ src/core/loop_structure/include/noelle/core/LoopStructure.hpp
@@ -19,6 +19,8 @@
  OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
  OR OTHER DEALINGS IN THE SOFTWARE.
  */
+#ifndef NOELLE_SRC_CORE_LOOPSTRUCTURE_H
+#define NOELLE_SRC_CORE_LOOPSTRUCTURE_H
 #pragma once
 
 #include "noelle/core/SystemHeaders.hpp"
@@ -107,3 +109,5 @@ private:
 };
 
 } // namespace llvm::noelle
+
+#endif
