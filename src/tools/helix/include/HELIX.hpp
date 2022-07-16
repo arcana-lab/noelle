8d68e54041e2b4c89c05439c8e1f727e6c384405
Tommy McMichen <mcmichen@u.northwestern.edu>
2022-07-16 12:35:46 -0500

    Added include guards to helix

--- src/tools/helix/include/HELIX.hpp
+++ src/tools/helix/include/HELIX.hpp
@@ -19,6 +19,8 @@
  OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
  OR OTHER DEALINGS IN THE SOFTWARE.
  */
+#ifndef NOELLE_SRC_TOOLS_HELIX_H
+#define NOELLE_SRC_TOOLS_HELIX_H
 #pragma once
 
 #include "noelle/core/SystemHeaders.hpp"
@@ -161,3 +163,5 @@ public:
 };
 
 } // namespace llvm::noelle
+
+#endif
--- src/tools/helix/include/SequentialSegment.hpp
+++ src/tools/helix/include/SequentialSegment.hpp
@@ -19,6 +19,8 @@
  OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
  OR OTHER DEALINGS IN THE SOFTWARE.
  */
+#ifndef NOELLE_SRC_TOOLS_SEQUENTIALSEGMENT_H
+#define NOELLE_SRC_TOOLS_SEQUENTIALSEGMENT_H
 #pragma once
 
 #include "HELIX.hpp"
@@ -86,3 +88,5 @@ private:
 };
 
 } // namespace llvm::noelle
+
+#endif
