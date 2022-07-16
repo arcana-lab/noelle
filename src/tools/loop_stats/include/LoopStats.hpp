5f88d67ce0a45f3aee13dbd1266ef34175370a9a
Tommy McMichen <mcmichen@u.northwestern.edu>
2022-07-16 12:34:09 -0500

    Added include guards to loop_stats

--- src/tools/loop_stats/include/LoopStats.hpp
+++ src/tools/loop_stats/include/LoopStats.hpp
@@ -19,6 +19,8 @@
  OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
  OR OTHER DEALINGS IN THE SOFTWARE.
  */
+#ifndef NOELLE_SRC_TOOLS_LOOPSTATS_H
+#define NOELLE_SRC_TOOLS_LOOPSTATS_H
 #pragma once
 
 #include "noelle/core/Noelle.hpp"
@@ -125,3 +127,5 @@ private:
 };
 
 } // namespace llvm::noelle
+
+#endif
