a8786a05e8fec81012aaafa865d4f75d91a3a426
Simone Campanoni <simo.xan@gmail.com>
2023-01-24 12:02:58 -0600

    Extracted the linker from noelle

--- src/core/linker/include/noelle/core/Linker.hpp
+++ src/core/linker/include/noelle/core/Linker.hpp
@@ -1,5 +1,5 @@
 /*
- * Copyright 2021 - 2022  Simone Campanoni
+ * Copyright 2021 - 2023  Simone Campanoni
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  of this software and associated documentation files (the "Software"), to deal
