739a94979ab1d3f84ca86966a861c011014627c1
Simone Campanoni <simo.xan@gmail.com>
2023-04-10 17:54:21 -0500

    Example: improved an example

--- examples/passes/environment/src/CatPass.cpp
+++ examples/passes/environment/src/CatPass.cpp
@@ -8,96 +8,111 @@
 
 #include "noelle/core/Noelle.hpp"
 
-using namespace llvm::noelle ;
+using namespace llvm::noelle;
 
 namespace {
 
-  struct CAT : public ModulePass {
-    static char ID; 
+struct CAT : public ModulePass {
+  static char ID;
 
-    CAT() : ModulePass(ID) {}
+  CAT() : ModulePass(ID) {}
 
-    bool doInitialization (Module &M) override {
-      return false;
-    }
+  bool doInitialization(Module &M) override {
+    return false;
+  }
 
-    bool runOnModule (Module &M) override {
+  bool runOnModule(Module &M) override {
 
-      /*
-       * Fetch NOELLE
-       */
-      auto& noelle = getAnalysis<Noelle>();
+    /*
+     * Fetch NOELLE
+     */
+    auto &noelle = getAnalysis<Noelle>();
 
-      /*
-       * Fetch the entry point.
-       */
-      auto fm = noelle.getFunctionsManager();
-      auto mainF = fm->getEntryFunction();
+    /*
+     * Fetch the entry point.
+     */
+    auto fm = noelle.getFunctionsManager();
+    auto mainF = fm->getEntryFunction();
 
-      /*
-       * fetch the loops with all their abstractions 
-       * (e.g., loop dependence graph, sccdag)
-       */
-      auto forest = noelle.getProgramLoopsNestingForest();
+    /*
+     * fetch the loops with all their abstractions
+     * (e.g., loop dependence graph, sccdag)
+     */
+    auto forest = noelle.getProgramLoopsNestingForest();
 
-      /*
-       * Print loop induction variables and invariant.
-       */
-      for (auto tree : forest->getTrees()){
-        for (auto loopNode : tree->getNodes()){
-          auto LS = loopNode->getLoop();
+    /*
+     * Print loop induction variables and invariant.
+     */
+    for (auto tree : forest->getTrees()) {
+      for (auto loopNode : tree->getNodes()) {
+        auto LS = loopNode->getLoop();
 
-          /*
-           * Print the first instruction the loop executes.
-           */
-          auto entryInst = LS->getEntryInstruction();
-          errs() << "Loop " << *entryInst << "\n";
+        /*
+         * Print the first instruction the loop executes.
+         */
+        auto entryInst = LS->getEntryInstruction();
+        errs() << "Loop " << *entryInst << "\n";
 
-          /*
-           * Print some information about the loop.
-           */
-          errs() << " Function = " << LS->getFunction()->getName() << "\n";
-          errs() << " Nesting level = " << LS->getNestingLevel() << "\n";
-          errs() << " This loop has " << loopNode->getNumberOfSubLoops() << " sub-loops (including sub-loops of sub-loops)\n";
+        /*
+         * Print some information about the loop.
+         */
+        errs() << " Function = " << LS->getFunction()->getName() << "\n";
+        errs() << " Nesting level = " << LS->getNestingLevel() << "\n";
+        errs() << " This loop has " << loopNode->getNumberOfSubLoops()
+               << " sub-loops (including sub-loops of sub-loops)\n";
 
-          /*
-           * Fetch the LoopDependenceInfo
-           */
-          auto loop = noelle.getLoop(LS);
+        /*
+         * Fetch the LoopDependenceInfo
+         */
+        auto loop = noelle.getLoop(LS);
 
-          /*
-           * Fetch the loop environment
-           */
-          auto loopEnv = loop->getEnvironment();
+        /*
+         * Fetch the loop environment
+         */
+        auto loopEnv = loop->getEnvironment();
 
-          /*
-           * Iterate over elements of the environment.
-           */
-          errs() << " Environment of the loop\n";
-          for (auto liveInOrOutValue : loopEnv->getProducers()){
-            errs() << "   " << *liveInOrOutValue << "\n";
-          }
+        /*
+         * Print the number of elements that compose the environment.
+         */
+        errs() << " Environment of the loop is composed by " << loopEnv->size()
+               << " elements\n";
+
+        /*
+         * Iterate over elements of the environment.
+         */
+        errs() << " Environment of the loop\n";
+        for (auto liveInOrOutValue : loopEnv->getProducers()) {
+          errs() << "   " << *liveInOrOutValue << "\n";
         }
       }
-   
-      return false;
     }
 
-    void getAnalysisUsage(AnalysisUsage &AU) const override {
-      AU.addRequired<Noelle>();
-    }
-  };
-}
+    return false;
+  }
+
+  void getAnalysisUsage(AnalysisUsage &AU) const override {
+    AU.addRequired<Noelle>();
+  }
+};
+} // namespace
 
 // Next there is code to register your pass to "opt"
 char CAT::ID = 0;
 static RegisterPass<CAT> X("CAT", "Simple user of the Noelle framework");
 
 // Next there is code to register your pass to "clang"
-static CAT * _PassMaker = NULL;
+static CAT *_PassMaker = NULL;
 static RegisterStandardPasses _RegPass1(PassManagerBuilder::EP_OptimizerLast,
-    [](const PassManagerBuilder&, legacy::PassManagerBase& PM) {
-        if(!_PassMaker){ PM.add(_PassMaker = new CAT());}}); // ** for -Ox
-static RegisterStandardPasses _RegPass2(PassManagerBuilder::EP_EnabledOnOptLevel0,
-    [](const PassManagerBuilder&, legacy::PassManagerBase& PM) {
-        if(!_PassMaker){ PM.add(_PassMaker = new CAT()); }}); // ** for -O0
+                                        [](const PassManagerBuilder &,
+                                           legacy::PassManagerBase &PM) {
+                                          if (!_PassMaker) {
+                                            PM.add(_PassMaker = new CAT());
+                                          }
+                                        }); // ** for -Ox
+static RegisterStandardPasses _RegPass2(
+    PassManagerBuilder::EP_EnabledOnOptLevel0,
+    [](const PassManagerBuilder &, legacy::PassManagerBase &PM) {
+      if (!_PassMaker) {
+        PM.add(_PassMaker = new CAT());
+      }
+    }); // ** for -O0
