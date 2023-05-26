83d90a367583e0e3106908e99d4b969671938169
TH Huang <tzhsn.huang@gmail.com>
2023-05-25 22:58:36 -0500

    Enabler: add the putc->putc_unlocked trans in io enabler

--- src/tools/input_output/src/InputOutput.cpp
+++ src/tools/input_output/src/InputOutput.cpp
@@ -27,11 +27,11 @@
 namespace llvm::noelle {
 
 std::unordered_map<std::string, std::string> stdioUnlockedFunctionMapping = {
-  { "fgetc", "fgetc_unlocked" },    { "fgets", "fgets_unlocked" },
-  { "fputc", "fputc_unlocked" },    { "fputs", "fputs_unlocked" },
-  { "fread", "fread_unlocked" },    { "fprintf", "fprintf_unlocked" },
-  { "fscanf", "fscanf_unlocked" },  { "getc", "getc_unlocked" },
-  { "getchar", "getchar_unlocked" }
+  { "fgetc", "fgetc_unlocked" },     { "fgets", "fgets_unlocked" },
+  { "fputc", "fputc_unlocked" },     { "fputs", "fputs_unlocked" },
+  { "fread", "fread_unlocked" },     { "fprintf", "fprintf_unlocked" },
+  { "fscanf", "fscanf_unlocked" },   { "getc", "getc_unlocked" },
+  { "getchar", "getchar_unlocked" }, { "putc", "putc_unlocked" },
 };
 
 InputOutput::InputOutput() : ModulePass{ ID } {}
