f71ee705efa78ce716e07b4814cf74313c5adc0d
Simone Campanoni <simo.xan@gmail.com>
2023-09-25 12:23:28 -0500

    Added a new example

---
+++ examples/passes/dependence_analysis/src/CatPass.cpp
@@ -0,0 +1,110 @@
+#include "llvm/Pass.h"
+#include "llvm/IR/Function.h"
+#include "llvm/Support/raw_ostream.h"
+#include "llvm/IR/LegacyPassManager.h"
+#include "llvm/Transforms/IPO/PassManagerBuilder.h"
+#include <algorithm>
+
+#include "noelle/core/Noelle.hpp"
+
+using namespace llvm::noelle;
+
+namespace {
+
+class MyDependenceAnalysis : public DataDependenceAnalysis {
+public:
+  MyDependenceAnalysis()
+    : DataDependenceAnalysis("Example of data dependence analysis"),
+      c{ 0 } {}
+
+  bool canThereBeAMemoryDataDependence(Instruction *fromInst,
+                                       Instruction *toInst,
+                                       Scope s) override {
+    auto f = fromInst->getFunction();
+    errs() << this->prefix << "canThereBeAMemoryDataDependence: Function "
+           << f->getName() << "\n";
+    errs() << this->prefix << "canThereBeAMemoryDataDependence:   From "
+           << *fromInst << "\n";
+    errs() << this->prefix << "canThereBeAMemoryDataDependence:   To "
+           << *toInst << "\n";
+    errs() << this->prefix << "canThereBeAMemoryDataDependence:   Scope " << s
+           << "\n";
+    errs() << this->prefix << "canThereBeAMemoryDataDependence:\n";
+
+    c++;
+
+    return true;
+  }
+
+  ~MyDependenceAnalysis() {
+    errs() << "The API has been invoked " << this->c << " times\n";
+  }
+
+private:
+  std::string prefix;
+  uint64_t c;
+};
+
+struct CAT : public ModulePass {
+  static char ID;
+
+  CAT() : ModulePass(ID) {}
+
+  bool doInitialization(Module &M) override {
+    return false;
+  }
+
+  bool runOnModule(Module &M) override {
+
+    /*
+     * Fetch NOELLE
+     */
+    auto &noelle = getAnalysis<Noelle>();
+
+    /*
+     * Register my data dependence analysis.
+     */
+    MyDependenceAnalysis myDepAnalysis{};
+    noelle.addAnalysis(&myDepAnalysis);
+
+    /*
+     * Fetch the PDG
+     */
+    auto PDG = noelle.getProgramDependenceGraph();
+
+    /*
+     * Fetch the FDG of "main"
+     */
+    auto fm = noelle.getFunctionsManager();
+    auto mainF = fm->getEntryFunction();
+    auto FDG = PDG->createFunctionSubgraph(*mainF);
+
+    return false;
+  }
+
+  void getAnalysisUsage(AnalysisUsage &AU) const override {
+    AU.addRequired<Noelle>();
+  }
+};
+} // namespace
+
+// Next there is code to register your pass to "opt"
+char CAT::ID = 0;
+static RegisterPass<CAT> X("CAT", "Simple user of the Noelle framework");
+
+// Next there is code to register your pass to "clang"
+static CAT *_PassMaker = NULL;
+static RegisterStandardPasses _RegPass1(PassManagerBuilder::EP_OptimizerLast,
+                                        [](const PassManagerBuilder &,
+                                           legacy::PassManagerBase &PM) {
+                                          if (!_PassMaker) {
+                                            PM.add(_PassMaker = new CAT());
+                                          }
+                                        }); // ** for -Ox
+static RegisterStandardPasses _RegPass2(
+    PassManagerBuilder::EP_EnabledOnOptLevel0,
+    [](const PassManagerBuilder &, legacy::PassManagerBase &PM) {
+      if (!_PassMaker) {
+        PM.add(_PassMaker = new CAT());
+      }
+    }); // ** for -O0
