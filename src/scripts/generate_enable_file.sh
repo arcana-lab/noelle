3bf55ae78155af75a99318a4b271a023cfc2ee15
Simone Campanoni <simo.xan@gmail.com>
2023-12-04 20:38:34 -0600

    Bug fixed

--- src/scripts/generate_enable_file.sh
+++ src/scripts/generate_enable_file.sh
@@ -8,13 +8,11 @@ fi
 mkdir -p $installDir ;
 
 # Set the enable file
-enableFile="${installDir}/enable" ;
+enableFile="../enable" ;
 
 echo "#!/bin/bash" > ${enableFile} ;
 echo "" >> ${enableFile} ;
 
-repoPath="`pwd`/../" ;
-noelleDir="`realpath ${repoPath}`" ;
-echo "NOELLE_HOME=${noelleDir}" >> ${enableFile} ;
+echo "NOELLE_HOME=${installDir}" >> ${enableFile} ;
 echo "export PATH=\${NOELLE_HOME}/install/bin:\$PATH" >> ${enableFile} ;
 echo "export LD_LIBRARY_PATH=\${NOELLE_HOME}/install/lib:\$LD_LIBRARY_PATH" >> ${enableFile} ;
