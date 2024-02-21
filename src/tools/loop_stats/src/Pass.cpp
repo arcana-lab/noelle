30fd080ccf00fb059410f439a68d220910708c3c
fsossai <federico.sossai@gmail.com>
2024-02-21 11:20:58 -0600

    Fixed `LoopStats` headers

--- src/tools/loop_stats/src/Pass.cpp
+++ src/tools/loop_stats/src/Pass.cpp
@@ -19,7 +19,7 @@
  OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
  OR OTHER DEALINGS IN THE SOFTWARE.
  */
-#include "LoopStats.hpp"
+#include "noelle/tools/LoopStats.hpp"
 
 using namespace llvm;
 using namespace arcana::noelle;
