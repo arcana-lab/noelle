f583603230e4c225d974de611775463d9f1e0aa7
Simone Campanoni <simo.xan@gmail.com>
2024-06-26 08:39:42 -0500

    [style]

--- src/tools/metadata_cleaner/src/MetadataCleaner.cpp
+++ src/tools/metadata_cleaner/src/MetadataCleaner.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016 - 2022  Yian Su, Simone Campanoni
+ * Copyright 2016 - 2024  Yian Su, Simone Campanoni
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  of this software and associated documentation files (the "Software"), to deal
@@ -23,7 +23,10 @@
 
 namespace arcana::noelle {
 
-MetadataCleaner::MetadataCleaner() : ModulePass{ ID }, cleanPDG{ false } {
+MetadataCleaner::MetadataCleaner() 
+  : ModulePass{ ID }
+  , cleanPDG{ false } {
+
   return;
 }
 
