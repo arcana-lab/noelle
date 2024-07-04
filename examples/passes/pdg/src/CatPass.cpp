cb1e7a16a5b31f97991d4c97f9bc04abee885698
Simone Campanoni <simo.xan@gmail.com>
2024-07-04 18:21:33 -0500

    [examples] Ported a new one

--- examples/passes/pdg/src/CatPass.cpp
+++ examples/passes/pdg/src/CatPass.cpp
@@ -1,31 +1,24 @@
-#include "llvm/Pass.h"
-#include "llvm/IR/Function.h"
-#include "llvm/Support/raw_ostream.h"
-#include "llvm/IR/LegacyPassManager.h"
-#include "llvm/Transforms/IPO/PassManagerBuilder.h"
-#include <algorithm>
-
 #include "arcana/noelle/core/NoellePass.hpp"
+#include "llvm/Analysis/GlobalsModRef.h"
+#include "llvm/Analysis/TypeBasedAliasAnalysis.h"
+#include "llvm/Analysis/ScopedNoAliasAA.h"
+#include "llvm/Analysis/ScalarEvolutionAliasAnalysis.h"
+#include "llvm/Analysis/CFLSteensAliasAnalysis.h"
+#include "llvm/Analysis/CFLAndersAliasAnalysis.h"
+#include "llvm/Analysis/ObjCARCAliasAnalysis.h"
 
 using namespace arcana::noelle;
 
 namespace {
 
-struct CAT : public ModulePass {
-  static char ID;
+struct CAT : public PassInfoMixin<CAT> {
 
-  CAT() : ModulePass(ID) {}
-
-  bool doInitialization(Module &M) override {
-    return false;
-  }
-
-  bool runOnModule(Module &M) override {
+  PreservedAnalyses run(Module &M, llvm::ModuleAnalysisManager &AM) {
 
     /*
      * Fetch NOELLE
      */
-    auto &noelle = getAnalysis<NoellePass>().getNoelle();
+    auto &noelle = AM.getResult<NoellePass>(M);
 
     /*
      * Fetch the PDG
@@ -97,32 +90,63 @@ struct CAT : public ModulePass {
         << "A Variable  = " << sizeof(VariableDependence<Value, Value>) << "\n";
     errs() << "A Memory  = " << sizeof(MemoryDependence<Value, Value>) << "\n";
 
-    return false;
-  }
-
-  void getAnalysisUsage(AnalysisUsage &AU) const override {
-    AU.addRequired<NoellePass>();
+    return PreservedAnalyses::all();
   }
 };
-} // namespace
 
 // Next there is code to register your pass to "opt"
-char CAT::ID = 0;
-static RegisterPass<CAT> X("CAT", "Simple user of the Noelle framework");
+AAManager NOELLE_createAliasAnalysesPipeline(void) {
+  AAManager AA;
 
-// Next there is code to register your pass to "clang"
-static CAT *_PassMaker = NULL;
-static RegisterStandardPasses _RegPass1(PassManagerBuilder::EP_OptimizerLast,
-                                        [](const PassManagerBuilder &,
-                                           legacy::PassManagerBase &PM) {
-                                          if (!_PassMaker) {
-                                            PM.add(_PassMaker = new CAT());
-                                          }
-                                        }); // ** for -Ox
-static RegisterStandardPasses _RegPass2(
-    PassManagerBuilder::EP_EnabledOnOptLevel0,
-    [](const PassManagerBuilder &, legacy::PassManagerBase &PM) {
-      if (!_PassMaker) {
-        PM.add(_PassMaker = new CAT());
-      }
-    }); // ** for -O0
+  AA.registerFunctionAnalysis<TypeBasedAA>();
+  AA.registerModuleAnalysis<GlobalsAA>();
+  AA.registerFunctionAnalysis<ScopedNoAliasAA>();
+  AA.registerFunctionAnalysis<SCEVAA>();
+  AA.registerFunctionAnalysis<CFLSteensAA>();
+  AA.registerFunctionAnalysis<CFLAndersAA>();
+  AA.registerFunctionAnalysis<llvm::objcarc::ObjCARCAA>();
+
+  return AA;
+}
+
+llvm::PassPluginLibraryInfo getPluginInfo() {
+  return {
+    LLVM_PLUGIN_API_VERSION,
+    "CAT",
+    LLVM_VERSION_STRING,
+    [](PassBuilder &PB) {
+      /*
+       * REGISTRATION FOR "opt -passes='PDGEmbedder'"
+       *
+       */
+      PB.registerPipelineParsingCallback(
+          [](StringRef Name,
+             llvm::ModulePassManager &PM,
+             ArrayRef<llvm::PassBuilder::PipelineElement>) {
+            if (Name == "CAT") {
+              PM.addPass(CAT());
+              return true;
+            }
+            return false;
+          });
+
+      /*
+       * REGISTRATION FOR "AM.getResult<NoellePass>()"
+       */
+      PB.registerAnalysisRegistrationCallback([](FunctionAnalysisManager &AM) {
+        AM.registerPass([&] { return NOELLE_createAliasAnalysesPipeline(); });
+      });
+
+      PB.registerAnalysisRegistrationCallback([](ModuleAnalysisManager &AM) {
+        AM.registerPass([&] { return NoellePass(); });
+      });
+    }
+  };
+}
+
+extern "C" LLVM_ATTRIBUTE_WEAK ::llvm::PassPluginLibraryInfo
+llvmGetPassPluginInfo() {
+  return getPluginInfo();
+}
+
+} // namespace
