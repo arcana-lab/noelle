e02c66820037f23bd33ddd788de0f56d35fd7f43
fsossai <federico.sossai@gmail.com>
2024-08-26 01:38:27 -0500

    Added `Pragmas.hpp`

---
+++ src/core/pragma_manager/include/arcana/noelle/core/Pragmas.hpp
@@ -0,0 +1,10 @@
+#ifndef __NOELLE_PRAGMAS_HPP__
+#define __NOELLE_PRAGMAS_HPP__
+
+template <typename... T>
+void noelle_pragma_begin(T...) {}
+
+template <typename... T>
+void noelle_pragma_end(T...) {}
+
+#endif
